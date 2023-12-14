bf32695ca2378b723221ec7bb6666276935d1396
Zaheer <{ID}+{username}@users.noreply.github.com>
2023-12-14 09:20:51 +0200

    wip adding routers

--- api/config/routers.py
+++ api/config/routers.py
@@ -2,16 +2,16 @@ from fastapi import APIRouter
 
 
 class BaseRouter(APIRouter):
-    def __init__(self, path: str, controller, *args, **kwargs):
+    def __init__(self, url: str, controller, *args, **kwargs):
         # https://stackoverflow.com/a/70563827
-        self.path = path
+        self.url = url
         self.controller = controller
-        self.router = APIRouter(*args, **kwargs)
+        self.router = APIRouter()
+        self.get(self.url)
 
-    def get(self, path, *args, **kwargs):
-        path = path or self.path
-        set_api_route = self.router.get(path, *args, **kwargs)
-
+    def get(self, url, *args, **kwargs):
+        url = url or self.url
+        set_api_route = self.router.get(url)
         set_api_route(self.controller.list)
 
 
@@ -21,4 +21,6 @@ class RouterUtils:
     def generate_routers(cls, app, url_routers):
         for router in url_routers:
             # router[1].prefix = router[0]
+            print("gggg")
+            print(router[1].__dict__)
             app.include_router(router[1].router)
--- api/core/urls.py
+++ api/core/urls.py
@@ -3,4 +3,4 @@ from config.routers import BaseRouter
 from core.controllers.user_controller import UserController
 
 # router = BaseRouter()
-router = BaseRouter(path="/core/user", UserController, tags=["user"])
+router = BaseRouter("/core/user", UserController, tags=["user"])
