895080f04727bcd63130e3d70667724afda80ebb
Zaheer <{ID}+{username}@users.noreply.github.com>
2023-12-17 17:28:21 +0200

    project restructure

---
+++ app/main.py
@@ -0,0 +1,1 @@
+zip_file_path = r"/home/timol/work/exploreai/udt/misc/CW_20231108_060001.gdb.zip"
---
+++ cwm/src/data_managers/base_data_manager.py
@@ -0,0 +1,3 @@
+class BaseDataManager:
+    def gdb_zip_to_layer(self, path: str, layer_name: str):
+        pass
