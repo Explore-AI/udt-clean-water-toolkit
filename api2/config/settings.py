0d3624c8b96aee4ffad6fc88c17d4af1e7d15ba6
Zaheer <{ID}+{username}@users.noreply.github.com>
2023-12-18 13:59:57 +0200

    updating api

--- api2/config/settings.py
+++ api2/config/settings.py
@@ -10,22 +10,28 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+if os.path.exists(os.path.join(BASE_DIR, ".env")):
+    from dotenv import load_dotenv
+
+    load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-len)w-+j25^2&s(=emv+_ky*9pa-&m5=p3!a@o1ong)=dyx9kp"
+SECRET_KEY = os.getenv("DJANGO_BASE_APP_SECRET_KEY__ENV_VAR")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv("DJANGO_BASE_APP_ALLOWED_HOSTS__ENV_VAR")]
 
 
 # Application definition
@@ -37,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "core",
 ]
 
 MIDDLEWARE = [
@@ -73,7 +80,16 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+x = print(os.getenv("DJANGO_BASE_APP_DEFAULT_DB_USER__ENV_VAR"))
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.postgresql_psycopg2",
+    #     "NAME": os.getenv("DJANGO_BASE_APP_DEFAULT_DB_NAME__ENV_VAR"),
+    #     "USER": os.getenv("DJANGO_BASE_APP_DEFAULT_DB_USER__ENV_VAR"),
+    #     "PASSWORD": os.getenv("DJANGO_BASE_APP_DEFAULT_DB_PASSWORD__ENV_VAR"),
+    #     "HOST": os.getenv("DJANGO_BASE_APP_DEFAULT_DB_HOST__ENV_VAR"),
+    #     "PORT": "5432",
+    # }
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
@@ -121,3 +137,38 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+DEFAULT_PAGE_SIZE = 100
+MAX_PAGE_SIZE = 1000
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "config.pagination.base_pagination.BasePagination",
+    "PAGE_SIZE": DEFAULT_PAGE_SIZE,
+}
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+}
+
+AUTH_USER_MODEL = "core.User"
+
+
+if os.path.exists(os.path.join(BASE_DIR, ".env")):
+    DEFAULT_RENDERER_CLASSES = ("rest_framework.renderers.JSONRenderer",)
+    # INSTALLED_APPS.append("corsheaders")
+    # MIDDLEWARE.append("corsheaders.middleware.CorsMiddleware")
+    MIDDLEWARE.append("django.middleware.common.CommonMiddleware")
+    CORS_ORIGIN_ALLOW_ALL = True
+    ALLOWED_HOSTS = ["*"]
+    DEBUG = True
