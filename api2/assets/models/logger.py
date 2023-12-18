65a2a8c7dd97d8030196ceb70041513a35dd8a69
Zaheer <{ID}+{username}@users.noreply.github.com>
2023-12-18 21:59:08 +0200

    add loger model

--- api2/assets/models/logger.py
+++ api2/assets/models/logger.py
@@ -1,15 +1,11 @@
-from django.db.models import Model, CharField, DateTimeField
+from django.db.models import Model, IntegerField, FloatField
 
 # from core.models.dma import Dma
 
 
 class Logger(Model):
-    gisid = CharField(max_length=50, null=False, blank=False)
-    date_created = DateTimeField(null=False, blank=False)
-    gps_x = CharField(max_length=50, null=False, blank=False)
-    gps_y = CharField(max_length=50, null=False, blank=False)
-    gps_z = CharField(max_length=50, null=False, blank=False)
-    shape_y = CharField(max_length=50, null=False, blank=False)
-    shape_x = CharField(max_length=50, null=False, blank=False)
+    gisid = IntegerField(null=False, blank=False)
+    shape_y = FloatField(max_length=50, null=False, blank=False)
+    shape_x = FloatField(max_length=50, null=False, blank=False)
     geometry = CharField(max_length=50, null=False, blank=False)
     # dma = ForeignKey(Dma, on_delete=CASCADE, related_name="dma_loggers")
