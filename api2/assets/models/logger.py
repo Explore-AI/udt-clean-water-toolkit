80daf0a5e344cd068a65b97b753ac3ac2f65ac2a
Zaheer <{ID}+{username}@users.noreply.github.com>
2023-12-18 22:06:34 +0200

    add loger model

--- api2/assets/models/logger.py
+++ api2/assets/models/logger.py
@@ -7,5 +7,5 @@ class Logger(Model):
     gisid = IntegerField(null=False, blank=False)
     shape_y = FloatField(max_length=50, null=False, blank=False)
     shape_x = FloatField(max_length=50, null=False, blank=False)
-    geometry = CharField(max_length=50, null=False, blank=False)
+    # geometry = CharField(max_length=50, null=False, blank=False) # this should be a geospation field
     # dma = ForeignKey(Dma, on_delete=CASCADE, related_name="dma_loggers")
