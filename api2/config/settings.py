5804ce7e56fcfbc7a711b3b517e16d27868d4868
Zaheer <{ID}+{username}@users.noreply.github.com>
2023-12-18 14:25:22 +0200

    updated settings

--- api2/config/settings.py
+++ api2/config/settings.py
@@ -80,20 +80,19 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-x = print(os.getenv("DJANGO_BASE_APP_DEFAULT_DB_USER__ENV_VAR"))
 DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.postgresql_psycopg2",
-    #     "NAME": os.getenv("DJANGO_BASE_APP_DEFAULT_DB_NAME__ENV_VAR"),
-    #     "USER": os.getenv("DJANGO_BASE_APP_DEFAULT_DB_USER__ENV_VAR"),
-    #     "PASSWORD": os.getenv("DJANGO_BASE_APP_DEFAULT_DB_PASSWORD__ENV_VAR"),
-    #     "HOST": os.getenv("DJANGO_BASE_APP_DEFAULT_DB_HOST__ENV_VAR"),
-    #     "PORT": "5432",
-    # }
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("DJANGO_BASE_APP_DEFAULT_DB_NAME__ENV_VAR"),
+        "USER": os.getenv("DJANGO_BASE_APP_DEFAULT_DB_USER__ENV_VAR"),
+        "PASSWORD": os.getenv("DJANGO_BASE_APP_DEFAULT_DB_PASSWORD__ENV_VAR"),
+        "HOST": os.getenv("DJANGO_BASE_APP_DEFAULT_DB_HOST__ENV_VAR"),
+        "PORT": "5432",
     }
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
 }
 
 
