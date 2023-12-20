fa0c1656ca9c5e065a1a0e88597b108d6c516834
AnnegretExplore <annegret@explore.ai>
2023-12-20 12:31:58 +0200

    update DMA model

--- api2/utilities/models/dma.py
+++ api2/utilities/models/dma.py
@@ -1,8 +1,5 @@
 from django.db.models import Model, CharField, JSONField
 
-#from django.contrib.gis.db.models import Model
-
-
 class DMA(Model):
     code=CharField(max_length=50)       #need to check max_length
     network_repr=JSONField()
