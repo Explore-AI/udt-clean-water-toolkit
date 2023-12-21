ccceee6ff8f5a8deeddfe87f33a43d6c72317544
AnnegretExplore <annegret@explore.ai>
2023-12-21 12:38:56 +0200

    wip asset models

--- api2/assets/models/hydrant.py
+++ api2/assets/models/hydrant.py
@@ -1,7 +1,12 @@
-from django.contrib.gis.db  import Model, IntegerField, FloatField
-# from core.models.dma import DMA
+from django.contrib.gis.db.models  import Model, IntegerField, FloatField, PointField, ForeignKey
+# from core.models.dma import DMA   #unsure
 
 class Hydrant(Model):
+    GISID = IntegerField(null=False, blank=False)
+    SHAPEX = FloatField(max_length=50, null=False, blank=False)
+    SHAPEY = FloatField(max_length=50, null=False, blank=False)
+    geometry = PointField()                                #unsure
+    DMACODE = ForeignKey(utilities.DMA,on_delete=CASCADE)  #unsure
 
     def __str__(self):
         return self.name
--- api2/assets/models/logger.py
+++ api2/assets/models/logger.py
@@ -1,12 +1,12 @@
-from django.contrib.gis.db import Model, IntegerField, FloatField
-# from core.models.dma import DMA
+from django.contrib.gis.db.models import Model, IntegerField, FloatField, PointField, ForeignKey
+# from core.models.dma import DMA1   #unsure    
 
 class Logger(Model):
-    gisid = IntegerField(null=False, blank=False)
-    shape_y = FloatField(max_length=50, null=False, blank=False)
-    shape_x = FloatField(max_length=50, null=False, blank=False)
-    # geometry = CharField(max_length=50, null=False, blank=False) # this should be a geospation field
-    # dma = ForeignKey(Dma, on_delete=CASCADE, related_name="dma_loggers")
+    GISID = IntegerField(null=False, blank=False)
+    SHAPEX = FloatField(max_length=50, null=False, blank=False)
+    SHAPEY = FloatField(max_length=50, null=False, blank=False)
+    geometry = PointField()                                  #unsure
+    DMACODE1 = ForeignKey(utilities.DMA1,on_delete=CASCADE)  #can possibly be created
     
     def __str__(self):
         return self.name
