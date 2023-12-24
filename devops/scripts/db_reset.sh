a876fe79bf6741a7c7c7b8b3115165d8b84de73d
Zaheer <{ID}+{username}@users.noreply.github.com>
2023-12-24 18:38:07 +0200

    added postgis db reset

---
+++ devops/scripts/db_reset.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Started udt DB reset. The udt database will be dropped and recreated. "
+
+DB_CONTAINER_ID=`docker ps | grep udtpostgis | grep postgis/postgis | awk '{ print $1 }'`
+
+#docker exec -it ${DB_CONTAINER_ID} psql --user udt -c "select pg_terminate_backend(pid) from pg_stat_activity where datname='udt_api2';"
+docker exec -it ${DB_CONTAINER_ID} psql --user udt -c "drop database udt"
+docker exec -it ${DB_CONTAINER_ID} psql --user udt -c "create database udt"
+
+echo "DB reset complete."
+
+# docker exec -it {DB_CONTAINER_ID} psql
