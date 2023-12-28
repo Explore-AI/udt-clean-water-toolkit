7cbb8da3dc41961493cc4a219574bb1edcbc4986
Zaheer <{ID}+{username}@users.noreply.github.com>
2023-12-28 12:39:26 +0200

    restructure

--- app_gdorm/analysis.py
+++ app_gdorm/analysis.py
@@ -8,14 +8,9 @@ from utilities.models import DMA
 # https://docs.djangoproject.com/en/4.2/ref/contrib/gis/geoquerysets/#std-fieldlookup-dwithin
 
 
+# https://networkx.org/documentation/stable/auto_examples/geospatial/plot_lines.html#sphx-glr-auto-examples-geospatial-plot-lines-py
 def analysis():
     print("success")
 
 
 analysis()
-
-
-# gdf = gdm.gdb_zip_to_gdf_layer(zip_file_path, "wLogger")
-
-# print(fiona.listlayers(zip_path))
-# ['wFitting', 'wNetworkMeter_Anno', 'wLogger', 'wNetworkOptValve', 'wCOCI_Select', 'wManifold', 'wConnectionMeter', 'wRemovedWaterDevice', 'wPressureContValve_Anno', 'wTrunkMain', 'wDistributionMain', 'wPreTreatmentMeter', 'wCOCI', 'wControlPillar', 'wConnectionMain', 'wVCAPLink', 'wAbandonedWaterDevice', 'wOnSiteProcessMain', 'wAbandonedWaterMain', 'wPressureFitting', 'wDuct', 'wCustomerValve', 'wPressureContValve', 'wVCAP', 'wHydrant_Anno', 'wBoundaryBox', 'wNetworkMeter', 'wNetworkOptValve_Anno', 'wHydrant', 'wMainsJobs', 'CW_Net_Junctions', 'wChamber', 'wOperationalSite', 'wConsumptionMeter', 'wEndItem', 'wRemovedWaterMain', 'wDistributionMain_Anno', 'wOperationalSite_Anno', 'wLINESTOPCLAMP', 'wTrunkMain_Anno', 'N_1_Desc', 'N_1_E0', 'N_1_E1', 'N_1_EDesc', 'N_1_EStatus', 'N_1_ETopo', 'N_1_FloDir', 'N_1_J0', 'N_1_J1', 'N_1_JDesc', 'N_1_JStatus', 'N_1_JTopo', 'N_1_JTopo2', 'N_1_Props']
