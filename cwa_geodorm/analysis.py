ecfa34fc9b8777a843ec2368f9368439e5b40abe
Zaheer <{ID}+{username}@users.noreply.github.com>
2024-01-08 08:28:20 +0200

    initial commit

--- cwa_geodorm/analysis.py
+++ cwa_geodorm/analysis.py
@@ -6,7 +6,7 @@ from cwa_geod.network import GisToGraphNetwork
 # https://github.com/ThamesWater/digitaltwin-refactor/blob/main/documentation/action-plan/action-plan-main.md
 # https://thameswater.visualstudio.com/Data%20Factory%20-%20Default/_wiki/wikis/Data-Factory---Default.wiki/5545/Digital-Twin-Overview
 # See examples folder
-def graph_from_trunk_mains():
+def clean_water_graph_from_gis_layers():
     gis_to_graph = GisToGraphNetwork(srid=DEFAULT_SRID)
 
     # for now it only creates the trunk mains networkx graph
@@ -16,7 +16,7 @@ def graph_from_trunk_mains():
 
 
 def main():
-    graph_from_trunk_mains()
+    clean_water_graph_from_gis_layers()
 
 
 main()
