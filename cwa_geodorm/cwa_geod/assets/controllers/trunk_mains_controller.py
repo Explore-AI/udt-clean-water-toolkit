43b464ea7da9502a39d687ca75d4c75b6582a268
Zaheer <{ID}+{username}@users.noreply.github.com>
2024-01-24 11:04:42 +0200

    update network controllers

--- cwa_geodorm/cwa_geod/assets/controllers/trunk_mains_controller.py
+++ cwa_geodorm/cwa_geod/assets/controllers/trunk_mains_controller.py
@@ -1,8 +1,17 @@
 from django.contrib.gis.db.models.functions import AsGeoJSON, Cast
 from django.db.models.functions import JSONObject
-from django.db.models import Value, JSONField
+from django.db.models import Value, JSONField, OuterRef
+from django.contrib.gis.measure import D
+from django.contrib.postgres.expressions import ArraySubquery
 from cleanwater.controllers import GeoDjangoController
-from ..models import TrunkMain
+from cwa_geod.assets.models import (
+    Logger,
+    TrunkMain,
+    Hydrant,
+    PressureFitting,
+    PressureControlValve,
+    Chamber,
+)
 from cwa_geod.config.settings import DEFAULT_SRID
 
 
@@ -20,7 +29,7 @@ class TrunkMainsController(GeoDjangoController):
 
     model = TrunkMain
     srid = DEFAULT_SRID
-    items_limit = 100000  # set default in config
+    # items_limit = 100000  # TODO: set default in config
     default_properties = [
         "id",
         "gisid",
@@ -28,6 +37,80 @@ class TrunkMainsController(GeoDjangoController):
         "dma_id",
         "dma__code",
     ]  # should not include the geometry column as per convention
+
+    def _generate_dwithin_subquery(self, qs, json_fields):
+        from django.contrib.gis.measure import D
+        from django.db.models.functions import JSONObject
+        from django.db.models import OuterRef
+
+        subquery = qs.filter(geometry__dwithin=(OuterRef("geometry"), D(m=1))).values(
+            json=JSONObject(**json_fields)
+        )
+        return subquery
+
+    def _generate_touches_subquery(self, qs, json_fields):
+        from django.contrib.gis.measure import D
+        from django.db.models.functions import JSONObject
+        from django.db.models import OuterRef
+
+        subquery = qs.filter(geometry__touches=OuterRef("geometry")).values(
+            json=JSONObject(**json_fields)
+        )
+        return subquery
+
+    def get_relation_to_point_assets_queryset(self):
+        json_fields = {
+            "id": "id",
+            "gisid": "gisid",
+            "geometry": "geometry",
+            "dma_id": "dma",
+            "dma_code": "dma__code",
+        }
+
+        # https://stackoverflow.com/questions/51102389/django-return-array-in-subquery
+        subquery1 = self._generate_touches_subquery(
+            TrunkMain.objects.all(), json_fields
+        )
+        subquery2 = self._generate_dwithin_subquery(Logger.objects.all(), json_fields)
+        subquery3 = self._generate_dwithin_subquery(Hydrant.objects.all(), json_fields)
+        subquery4 = self._generate_dwithin_subquery(
+            PressureFitting.objects.all(), json_fields
+        )
+
+        subquery5 = PressureControlValve.objects.filter(
+            geometry__dwithin=(OuterRef("geometry"), D(m=1))
+        ).values(
+            json=JSONObject(
+                id="id",
+                gisid="gisid",
+                geometry="geometry",
+                dma_1_id="dma_1",
+                dma_2_id="dma_2",
+                dma_1_code="dma_1__code",
+                dma_2_code="dma_1__code",
+            )
+        )
+
+        subquery6 = Chamber.objects.filter(
+            geometry__dwithin=(OuterRef("geometry"), D(m=1))
+        ).values(
+            json=JSONObject(
+                id="id",
+                gisid="gisid",
+                geometry="geometry",
+            )
+        )
+
+        qs = TrunkMain.objects.annotate(
+            trunk_mains_data=ArraySubquery(subquery1),
+            logger_data=ArraySubquery(subquery2),
+            hydrant_data=ArraySubquery(subquery3),
+            pressure_fitting_data=ArraySubquery(subquery4),
+            pressure_valve_data=ArraySubquery(subquery5),
+            chamber_data=ArraySubquery(subquery6),
+        )
+
+        return qs
 
     def get_geometry_queryset(self, properties=None):
         properties = properties or self.default_properties
@@ -53,7 +136,7 @@ class TrunkMainsController(GeoDjangoController):
     def trunk_mains_to_geojson(self, properties=None):
         """Serialization of db data to GeoJSON.
 
-        Fast (maybe with bigger datasets) serialization into geoson.
+        Faster (with bigger datasets) serialization into geoson.
 
         Params:
               properties: list (optional). A list of model fields
@@ -66,8 +149,8 @@ class TrunkMainsController(GeoDjangoController):
 
     def trunk_mains_to_geojson2(self, properties=None):
         """Serialization of db data to GeoJSON. Alternate method
-        Slighly faster serialization into geoson compared to 1) but
-        employs iteration.
+        Slighly faster serialization into geoson compared to 1) for smaller
+        data sets but employs iteration.
 
         Params:
               properties: list (optional). A list of model fields
--- cwa_geodorm/cwa_geod/network/gis_to_graph_network.py
+++ cwa_geodorm/cwa_geod/network/gis_to_graph_network.py
@@ -20,26 +20,6 @@ class GisToGraphNetwork(NetworkController):
 
         return trunk_mains_nx
 
-    def _generate_dwithin_subquery(self, qs, json_fields):
-        from django.contrib.gis.measure import D
-        from django.db.models.functions import JSONObject
-        from django.db.models import OuterRef
-
-        subquery = qs.filter(geometry__dwithin=(OuterRef("geometry"), D(m=1))).values(
-            json=JSONObject(**json_fields)
-        )
-        return subquery
-
-    def _generate_touches_subquery(self, qs, json_fields):
-        from django.contrib.gis.measure import D
-        from django.db.models.functions import JSONObject
-        from django.db.models import OuterRef
-
-        subquery = qs.filter(geometry__touches=OuterRef("geometry")).values(
-            json=JSONObject(**json_fields)
-        )
-        return subquery
-
     def create_network2(self):
         qs = self._get_trunk_mains_data()
         import pdb
@@ -47,72 +27,8 @@ class GisToGraphNetwork(NetworkController):
         pdb.set_trace()
 
     def _get_trunk_mains_data(self):
-        from django.contrib.gis.measure import D
-        from django.contrib.postgres.expressions import ArraySubquery
-        from django.db.models import OuterRef
-        from django.db.models.functions import JSONObject
-        from cwa_geod.assets.models import (
-            Logger,
-            DistributionMain,
-            TrunkMain,
-            Hydrant,
-            PressureFitting,
-            PressureControlValve,
-            Chamber,
-        )
-
-        qs = TrunkMain.objects.union(DistributionMain.objects.all())
-
-        json_fields = {
-            "id": "id",
-            "gisid": "gisid",
-            "geometry": "geometry",
-            "dma_id": "dma",
-            "dma_code": "dma__code",
-        }
-
-        # https://stackoverflow.com/questions/51102389/django-return-array-in-subquery
-        subquery1 = self._generate_touches_subquery(qs, json_fields)
-        subquery2 = self._generate_dwithin_subquery(Logger.objects.all(), json_fields)
-        subquery3 = self._generate_dwithin_subquery(Hydrant.objects.all(), json_fields)
-        subquery4 = self._generate_dwithin_subquery(
-            PressureFitting.objects.all(), json_fields
-        )
-
-        subquery5 = PressureControlValve.objects.filter(
-            geometry__dwithin=(OuterRef("geometry"), D(m=1))
-        ).values(
-            json=JSONObject(
-                id="id",
-                gisid="gisid",
-                geometry="geometry",
-                dma_1_id="dma_1",
-                dma_2_id="dma_2",
-                dma_1_code="dma_1__code",
-                dma_2_code="dma_1__code",
-            )
-        )
-
-        subquery6 = Chamber.objects.filter(
-            geometry__dwithin=(OuterRef("geometry"), D(m=1))
-        ).values(
-            json=JSONObject(
-                id="id",
-                gisid="gisid",
-                geometry="geometry",
-            )
-        )
-
-        qs = qs.annotate(
-            trunk_mains_data=ArraySubquery(subquery1),
-            logger_data=ArraySubquery(subquery2),
-            hydrant_data=ArraySubquery(subquery3),
-            pressure_fitting_data=ArraySubquery(subquery4),
-            pressure_valve_data=ArraySubquery(subquery5),
-            chamber_data=ArraySubquery(subquery6),
-        )
-
-        return qs
+        tm = TrunkMainsController()
+        return tm.get_relation_to_point_assets_queryset()
 
     def _create_trunk_mains_graph(self):
         tm = TrunkMainsController()
