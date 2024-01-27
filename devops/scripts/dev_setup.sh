a169e3bb83f5bddbde2ef338ec561236192d303f
zaheer <ztimol@users.noreply.github.com>
2024-01-27 16:54:51 +0200

    merge

--- devops/scripts/dev_setup.sh
+++ devops/scripts/dev_setup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-echo "Setting up the cwa_geodorm app for development."
+echo "Setting up the cwa_geodjango app for development."
 echo "The following actions will take place:"
 echo "udt postgis server will be setup."
 echo "A udt postgis database will be created."
 echo "A udt superuser will be created on the postgis database."
-echo "pip packages will be installed for the cwa_geodorm app."
+echo "pip packages will be installed for the cwa_geodjango app."
 echo "pip packages will be installed for the cwm in dev mode."
 echo
 
-docker-compose -f ../docker/docker-compose-postgis.yml -f ../docker/docker-compose-cwa-geodorm-dev.yml up -d
+docker-compose -f ../../docker/docker-compose-postgis.yml -f ../../docker/docker-compose-cwa-geodorm-dev.yml up -d
 
-CWA_GEODORM_CONTAINER_ID=`docker ps | grep udtcwageodormdev | grep cwa_geodorm_dev | awk '{ print $1 }'`
+CWA_GEODORM_CONTAINER_ID=`docker ps | grep udtcwageodjangodev | grep cwa_geodjango_dev | awk '{ print $1 }'`
 
 docker exec -it ${CWA_GEODORM_CONTAINER_ID} pip install -r requirements.txt -r dev-requirements.txt
 
@@ -22,4 +22,4 @@ docker exec -it ${CWA_GEODORM_CONTAINER_ID} pip install -e ../../cwm/
 #docker exec -it ${CWA_GEODORM_CONTAINER_ID} python3 main.py migrate
 
 echo
-echo "cwa_geodeom app dev setup complete."
+echo "cwa_geodjango app dev setup complete."
