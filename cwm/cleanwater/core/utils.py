e0efdf808af64e8a445d482f304f5c8d676bece9
Zaheer <{ID}+{username}@users.noreply.github.com>
2024-01-29 16:46:28 +0200

    wip: fix to distances

--- cwm/cleanwater/core/utils.py
+++ cwm/cleanwater/core/utils.py
@@ -10,7 +10,6 @@ def normalised_point_position_on_line(line_geom, point_geom, srid=None):
     start_of_line_point = Point(line_geom.coords[0][0], srid=srid)
 
     # https://zach.se/geodesic-distance-between-points-in-geodjango/
-    # TODO: fix below distane calc as not geodesic
     # https://docs.djangoproject.com/en/5.0/ref/contrib/gis/geos/
     normalised_position_on_line = 1 - (
         (line_geom.length - start_of_line_point.distance(point_geom)) / line_geom.length
