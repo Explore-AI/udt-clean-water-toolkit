6a142a23d6cca418e740355cf1d7283ebbda9e95
zaheer <ztimol@users.noreply.github.com>
2024-02-08 11:18:33 +0200

    cleanup

--- cwa/cwa_geodjango/analysis.py
+++ cwa/cwa_geodjango/analysis.py
@@ -4,8 +4,6 @@ from cwa_geod.config.settings import DEFAULT_SRID
 from cwa_geod.network import GisToNetworkX
 #    from cwa_geod.config.db.graph_db import init_graphdb
 
-# https://github.com/ThamesWater/digitaltwin-refactor/blob/main/documentation/action-plan/action-plan-main.md
-# https://thameswater.visualstudio.com/Data%20Factory%20-%20Default/_wiki/wikis/Data-Factory---Default.wiki/5545/Digital-Twin-Overview
 # See examples folder
 
 #@init_graphdb
@@ -21,9 +19,7 @@ def cleanwater_gis2nx():
 # TODO: Deprecated. for test purposes only.
 def create_pipes_network():
     # for now it only creates the trunk mains networkx graph
-    # nx_graph = gis_to_graph.create_network()
 
-    # new trial approach to create network
     gis_to_nx = GisToNetworkX(srid=DEFAULT_SRID)
     gis_to_nx.create_network()
 
