c153bbdbb0bdbf452aefed363e8a6dad3f6aced9
Mudiswa Muteba <mudi@Mudiswas-MacBook-Air.local>
2024-02-21 17:53:34 +0200

    switch to match assertion

--- cwm/tests/test_geodjango_data_manager.py
+++ cwm/tests/test_geodjango_data_manager.py
@@ -10,19 +10,16 @@ class TestGeoDjangoDataManager:
         geodjango_data_manager = GeoDjangoDataManager()
         invalid_zip_path = "./invalid.zip"
         valid_layer_name = "a valid layer name"  # to replace with real valid layer name
-
-        with pytest.raises(Exception) as exc_info:
+        
+        error_msg = "gdf file not found"
+        with pytest.raises(Exception, match=error_msg):
             geodjango_data_manager.gdb_zip_to_gdf_layer(invalid_zip_path, valid_layer_name)
-            assert exc_info.value.args[0] == "gdf file not found"
 
     def test_arg_invalid_layer_name(self):
         geodjango_data_manager = GeoDjangoDataManager()
         valid_zip_path = "a valid path"  # to replace with real valid path
         invalid_layer_name = "invalid_layer"
-
-        with pytest.raises(LayerLoadException) as exc_info:
+        
+        error_msg = f"Layer cannot be identified with provided name: {invalid_layer_name}"
+        with pytest.raises(LayerLoadException, match=error_msg):
             geodjango_data_manager.gdb_zip_to_gdf_layer(valid_zip_path, invalid_layer_name)
-            assert (
-                exc_info.value.args[0]
-                == f"Layer cannot be identified with provided name: {invalid_layer_name}"
-            )
