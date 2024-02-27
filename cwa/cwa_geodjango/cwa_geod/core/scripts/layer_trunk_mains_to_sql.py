196b4ed7d3531a914ca0d2bc0ee72f222fbff000
Zaheer <{ID}+{username}@users.noreply.github.com>
2024-02-27 10:09:30 +0200

    update trunk mains script

--- cwa/cwa_geodjango/cwa_geod/core/scripts/layer_trunk_mains_to_sql.py
+++ cwa/cwa_geodjango/cwa_geod/core/scripts/layer_trunk_mains_to_sql.py
@@ -4,7 +4,7 @@ from django.contrib.gis.gdal.error import GDALException
 from cwa_geod.assets.models import TrunkMain
 from cwa_geod.utilities.models import DMA
 
-TRUNK_MAINS_LAYER_INDEX = 9
+TRUNK_MAINS_LAYER_INDEX = 4
 DMA_FIELD_NAME = "DMACODE"
 
 
@@ -13,74 +13,30 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument("-f", "--file", type=str, help="Path to gdb.zip")
+        parser.add_argument("-x", "--index", type=str, help="Layer index")
 
     ### This method doesn't work because of errors in the geometry object
-    # def handle(self, *args, **kwargs):
-    #     zip_path = kwargs.get("file")
-
-    #     ds = DataSource(zip_path)
-    #     trunk_main_layer = ds[TRUNK_MAINS_LAYER_INDEX]
-
-    #     layer_gisids = trunk_main_layer.get_fields("GISID")
-    #     layer_dma_codes = trunk_main_layer.get_fields("DMACODE")
-    #     layer_geometries = trunk_main_layer.get_geoms()
-
-    #     new_trunk_mains = []
-    #     for (
-    #         layer_gisid,
-    #         layer_dma_code_1,
-    #         layer_geometry,
-    #     ) in zip(
-    #         layer_gisids,
-    #         layer_dma_codes,
-    #         layer_geometries,
-    #     ):
-    #         data = {
-    #             "gisid": layer_gisid,
-    #             "dma": layer_dma_code_1,
-    #             "geometry": layer_geometry.wkt,
-    #         }
-
-    #         if not None in data.values():
-    #             dma = DMA.objects.get(code=data["dma"])
-    #             data["dma"] = dma
-    #             new_trunk_mains.append(TrunkMain(**data))
-
-    #     TrunkMain.objects.bulk_create(new_trunk_mains)
-
     def handle(self, *args, **kwargs):
         zip_path = kwargs.get("file")
+        layer_index = kwargs.get("layer_index")
 
         ds = DataSource(zip_path)
-        trunk_mains_layer = ds[TRUNK_MAINS_LAYER_INDEX]
+        trunk_main_layer = ds[4]
 
-        print(
-            f"There are {trunk_mains_layer.num_feat} features. Large numbers of features will take a long time to save."
-        )
+        layer_gis_ids = trunk_main_layer.get_fields("GISID")
+        layer_geometries = trunk_main_layer.get_geoms()
 
         new_trunk_mains = []
-        for feature in trunk_mains_layer:
-            gisid = feature.get("GISID")
-            dma_code = feature.get(DMA_FIELD_NAME)
-            shape_length = feature.get("SHAPE_Length")
+        for gid, geometry in zip(layer_gis_ids, layer_geometries):
+            dmas = DMA.objects.filter(geometry_multipolygon__intersects=geometry)
 
-            # Had to to the except as got this error
-            # django.contrib.gis.gdal.error.GDALException: Invalid OGR Integer Type: 11
-            try:
-                geometry = feature.geom
-            except GDALException:
-                continue
+            new_trunk_main = TrunkMain(gid=gid, geometry=geometry, dma=dmas)
+            new_trunk_mains.append(new_trunk_main)
 
-            data = {
-                "gisid": gisid,
-                "dma": dma_code,
-                "shape_length": shape_length,
-                "geometry": geometry.wkt,
-            }
+            if len(new_trunk_mains) == 100000:
+                TrunkMain.objects.bulk_create(new_trunk_mains)
+                new_trunk_mains = []
 
-            if not None in data.values():
-                dma = DMA.objects.get(code=data["dma"])
-                data["dma"] = dma
-                new_trunk_mains.append(TrunkMain(**data))
-
-        TrunkMain.objects.bulk_create(new_trunk_mains)
+        # save the last set of data as it will probably be less than 100000
+        if new_trunk_mains:
+            TrunkMain.objects.bulk_create(new_trunk_mains)
