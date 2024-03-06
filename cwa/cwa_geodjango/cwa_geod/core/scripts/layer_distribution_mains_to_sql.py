3625528be2b611017da09d2791807c3d8f8ba651
Zaheer <{ID}+{username}@users.noreply.github.com>
2024-03-06 22:05:36 +0200

    rever to previous distmain script as its faster

--- cwa/cwa_geodjango/cwa_geod/core/scripts/layer_distribution_mains_to_sql.py
+++ cwa/cwa_geodjango/cwa_geod/core/scripts/layer_distribution_mains_to_sql.py
@@ -1,49 +1,42 @@
 from django.core.management.base import BaseCommand
 from django.contrib.gis.gdal import DataSource
-from django.contrib.gis.utils import LayerMapping
 from cwa_geod.assets.models import DistributionMain
 from cwa_geod.utilities.models import DMA
-from django.db import IntegrityError
-from django.core.exceptions import ValidationError
 
 
 class Command(BaseCommand):
-    help = "Write Thames Water distribution mains layer data to sql"
+    help = "Write Thames Water distritribition mains layer data to sql"
 
     def add_arguments(self, parser):
-        parser.add_argument("-f", "--file", type=str, help="Path to valid datasource")
+        parser.add_argument("-f", "--file", type=str, help="Path to gdb.zip")
         parser.add_argument("-x", "--index", type=int, help="Layer index")
 
     def handle(self, *args, **kwargs):
-        ds_path = kwargs.get("file")
+        zip_path = kwargs.get("file")
         layer_index = kwargs.get("index")
-        ds = DataSource(ds_path)
+
+        ds = DataSource(zip_path)
+        distribution_mains_layer = ds[layer_index]
+
         print(
-            f"""There are {ds[layer_index].num_feat} features.
-Large numbers of features will take a long time to save."""
-        )
-        layer_mapping = {
-            "gid": "GISID",
-            "geometry": "MULTILINESTRING",
-        }
-        lm = LayerMapping(
-            DistributionMain, ds, layer_mapping, layer=layer_index, transform=False
+            f"There are {distribution_mains_layer.num_feat} features. Large numbers of features will take a long time to save."
         )
 
-        lyr = ds[layer_index]
-        for feat in lyr:
-            gid = feat.get("GISID")
-            if DistributionMain.objects.filter(gid=gid).exists():
-                continue
+        new_distribution_mains = []
+        for feature in distribution_mains_layer:
+            gisid = feature.get("GISID")
 
-            try:
-                lm.save(strict=True, fid_range=(feat.fid, feat.fid + 1), step=1000000)
-            except IntegrityError as err:
-                if "identifier" in str(err):
-                    raise ValidationError(
-                        {"identifier": "This identifier is already in use."}
-                    ) from err
-                raise err
+            new_distribution_mains.append(
+                DistributionMain(gid=gisid, geometry=geometry.wkt)
+            )
+
+            if len(new_distribution_mains) == 100000:
+                DistributionMain.objects.bulk_create(new_distribution_mains)
+                new_distribution_mains = []
+
+            # save the last set of data as it will probably be less than 100000
+            if new_distribution_mains:
+                DistributionMain.objects.bulk_create(new_distribution_mains)
 
         for distribution_main in DistributionMain.objects.only("id", "geometry"):
             wkt = distribution_main.geometry.wkt
