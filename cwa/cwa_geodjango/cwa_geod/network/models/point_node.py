82afc9302af5efce2adbd5555783829ea3b1bf5c
GerhardtB <gerhardtbotha5@gmail.com>
2024-03-12 11:32:23 +0200

    Update: cardinality set to ZeroOrOne

--- cwa/cwa_geodjango/cwa_geod/network/models/point_node.py
+++ cwa/cwa_geodjango/cwa_geod/network/models/point_node.py
@@ -1,19 +1,18 @@
 from neomodel import (
     StructuredNode,
-    Relationship,
+    RelationshipTo,
     IntegerProperty,
     JSONProperty,
 )
 from neomodel.contrib.spatial_properties import PointProperty
 from .trunk_main import TrunkMain
 from .distribution_main import DistributionMain
+from neomodel.cardinality import ZeroOrOne
 
 
 class PointNode(StructuredNode):
     gid = IntegerProperty(unique_index=True, required=True)
     dmas = JSONProperty(required=True)
     location = PointProperty(crs="wgs-84")
-    trunk_main = Relationship("PointNode", "TRUNKMAIN", model=TrunkMain)
-    distribution_main = Relationship(
-        "PointNode", "DISTRIBUTIONMAIN", model=DistributionMain
-    )
+    trunk_main = RelationshipTo("PointNode", "TRUNKMAIN", model=TrunkMain, cardinality=ZeroOrOne)
+    distribution_main = RelationshipTo("PointNode", "DISTRIBUTIONMAIN", model=DistributionMain, cardinality=ZeroOrOne)
