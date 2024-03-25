86b33072ef6ad3d0f51a665ae9ca366a640a4b2a
gerald-eai <gerald@explore.ai>
2024-03-25 15:32:34 +0200

    Updated utility models to use sqlalchemy orm v2 syntax

--- cwa/cwa_geoalchemy/cwageolachemy/utilities/models/dma.py
+++ cwa/cwa_geoalchemy/cwageolachemy/utilities/models/dma.py
@@ -4,19 +4,30 @@ from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime, J
 from sqlalchemy.orm import relationship
 from geoalchemy2 import Geometry
 
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import Optional
+from datetime import datetime
+
 
 class DMA(Base):
     
     __tablename__ = "utilities_dma"
 
-    code = Column(String, primary_key=True)
-    name = Column(String, nullable=False)
-    # utility_id = Column(Integer, ForeignKey("utility.id"), nullable=False)
-    # utility = relationship("Utility", back_populates="utility_dmas")
-    network_repr = Column(JSON, nullable=True)
-    geometry = Column(
-        Geometry(geometry_type="MULTIPOLYGON", srid=27700, spatial_index=True),
-        nullable=False,
-    )
-    modified_at = Column(DateTime, nullable=False)
-    created_at = Column(DateTime, nullable=False)
+    # code = Column(String, primary_key=True)
+    # name = Column(String, nullable=False)
+    # # utility_id = Column(Integer, ForeignKey("utility.id"), nullable=False)
+    # # utility = relationship("Utility", back_populates="utility_dmas")
+    # network_repr = Column(JSON, nullable=True)
+    # geometry = Column(
+    #     Geometry(geometry_type="MULTIPOLYGON", srid=27700, spatial_index=True),
+    #     nullable=False,
+    # )
+    # modified_at = Column(DateTime, nullable=False)
+    # created_at = Column(DateTime, nullable=False)
+    
+    code: Mapped[str] = mapped_column(String, primary_key=True)
+    name: Mapped[str] = mapped_column(String(255), nullable=False)
+    network_repr: Optional[Mapped[dict]] = mapped_column(JSON, nullable=True)
+    geometry: Mapped[Geometry] = mapped_column(Geometry(geometry_type="MULTIPOLYGON", srid=27700, spatial_index=True))
+    modified_at: Mapped[datetime] = mapped_column(DateTime)
+    created_at: Mapped[datetime] = mapped_column(DateTime)
--- cwa/cwa_geoalchemy/cwageolachemy/utilities/models/utility.py
+++ cwa/cwa_geoalchemy/cwageolachemy/utilities/models/utility.py
@@ -1,12 +1,18 @@
 from ...core.db.postgis_db import Base
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime 
-from sqlalchemy.orm import relationship
-from geoalchemy2 import Geometry
+from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
 
 class Utility(Base): 
     __tablename__="utilities_utility"
     
-    id = Column(Integer, primary_key=True)
-    name = Column(String(255), nullable=False, unique=True)
-    modified_at = Column(DateTime, nullable=False)
-    created_at = Column(DateTime, nullable=False)
+    # id = Column(Integer, primary_key=True)
+    # name = Column(String(255), nullable=False, unique=True)
+    # modified_at = Column(DateTime, nullable=False)
+    # created_at = Column(DateTime, nullable=False)
+    
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(255))
+    modified_at: Mapped[datetime] = mapped_column(DateTime)
+    created_at: Mapped[datetime] = mapped_column(DateTime)
+    
