62c0c825a7410b17cef8059edddc12a7f2248f2a
zaheer <ztimol@users.noreply.github.com>
2024-04-01 19:41:21 +0200

    wip: refactor

--- cwa/cwa_geodjango/cwa_geod/network/models/pipe_end.py
+++ cwa/cwa_geodjango/cwa_geod/network/models/pipe_end.py
@@ -1,11 +1,10 @@
-from neomodel import StringProperty, IntegerProperty
+from neomodel import IntegerProperty
 from .point_node import PointNode
-from cwa_geod.core.constants import PIPE_END__NAME, PIPE_ASSETS__CHOICES
+from cwa_geod.core.constants import PIPE_END__NAME
 
 
 class PipeEnd(PointNode):
     gid = IntegerProperty(index=True, required=True)
-    # pipe_type = StringProperty(required=True, index=True, choices=PIPE_ASSETS__CHOICES)
 
     class AssetMeta:
         node_type = PIPE_END__NAME
