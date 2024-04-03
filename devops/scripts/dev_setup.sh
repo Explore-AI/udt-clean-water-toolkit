270b0bb3fb1c1e98b750b635ee4116fce81be0f5
spatialgeobyte <158478685+spatialgeobyte@users.noreply.github.com>
2024-04-03 16:12:42 +0200

    Add geoserver settings

--- devops/scripts/dev_setup.sh
+++ devops/scripts/dev_setup.sh
@@ -9,10 +9,12 @@ echo "pip packages will be installed for the cwa_geodjango app."
 echo "pip packages will be installed for the cwm in dev mode."
 echo
 
-docker compose -f ../docker/docker-compose-postgis.yml -f ../docker/docker-compose-neo4j.yml -f ../docker/docker-compose-cwa-geodjango-dev.yml -f ../docker/docker-compose-cwa-geoalchemy-dev.yml up -d --build
+
+docker compose -f ../docker/docker-compose-postgis.yml -f ../docker/docker-compose-neo4j.yml -f ../docker/docker-compose-cwa-geodjango-dev.yml -f ../docker/docker-compose-cwa-geoalchemy-dev.yml -f ../docker/docker-compose-geoserver.yml up -d --build
 
 CWA_GEODORM_CONTAINER_ID=`docker ps | grep udtcwageodjangodev | grep cwa_geodjango_dev | awk '{ print $1 }'`
 CWA_GEOALCHEMY_CONTAINER_ID=`docker ps | grep udtcwageoalchemydev | grep cwa_geoalchemy_dev | awk '{ print $1 }'`
+CWA_GEOSERVER_CONTAINER_ID=`docker ps | grep udtgeoserver | grep kartoza/geoserver:2.25.0 | awk '{ print $1 }'`
 
 docker exec -it ${CWA_GEODORM_CONTAINER_ID} pip install -r requirements.txt -r dev-requirements.txt
 
@@ -24,6 +26,8 @@ docker exec -it ${CWA_GEODORM_CONTAINER_ID} pip install -e ../../cwm/
 
 #docker exec -it ${CWA_GEODORM_CONTAINER_ID} python3 main.py migrate
 
+docker exec -it ${CWA_GEODORM_CONTAINER_ID} /bin/bash /geoserver/geoserver_import.sh
 echo
 echo "cwa_geodjango app dev setup complete."
 echo "cwa_geoalchemy app dev setup complete."
+echo "GeoServer app setup complete."
---
+++ devops/scripts/geoserver/geoserver_populate_services.py
@@ -0,0 +1,130 @@
+import sys
+from os import environ
+import psycopg2
+from geo.Geoserver import Geoserver
+from requests import get, put, exceptions
+from requests.auth import HTTPBasicAuth
+
+
+class Importer:
+    def __init__(self):
+        self.default = {
+            'GEO_USER': environ.get('GEOSERVER_ADMIN_USER', 'admin'),
+            'GEO_PASS': environ.get('GEOSERVER_ADMIN_PASSWORD'),
+            'GEO_WORKSPACE': environ.get('GEOSERVER_WORKSPACE', 'udt'),
+            'GEOSERVER_INSTANCE_URL': environ.get('GEOSERVER_URL', 'http://localhost:8080/geoserver'),
+            'DATABASE_HOST': environ.get('HOST'),
+            'DATABASE_USER': environ.get('POSTGRES_USER'),
+            'DATABASE_PASSWORD': environ.get('POSTGRES_PASS'),
+            'DATABASE_NAME': environ.get('POSTGRES_DB')
+
+        }
+    # Function to create a workspace in GeoServer
+    def publish_workspace(self, geoserver_site_url, username, user_pass, workspace_name):
+        geo = Geoserver(geoserver_site_url, username='%s' % username, password='%s' % user_pass)
+
+        auth = HTTPBasicAuth('%s' % username, '%s' % user_pass)
+        try:
+            rest_url = '%s/rest/workspaces/%s.json' % (geoserver_site_url, workspace_name)
+            response = get(rest_url, auth=auth)
+            response.raise_for_status()
+        except exceptions.HTTPError:
+            geo.create_workspace(workspace='%s' % workspace_name)
+            geo.set_default_workspace('%s' % workspace_name)
+
+    # Function to create a store in GeoServer
+    def publish_store(self, geoserver_site_url, username, user_pass, workspace_name, pg_host, pg_user, pg_pass,
+                      pg_name):
+        geo = Geoserver(geoserver_site_url, username='%s' % username, password='%s' % user_pass)
+        auth = HTTPBasicAuth('%s' % username, '%s' % user_pass)
+        try:
+            rest_url = '%s/rest/workspaces/%s/datastores/%s.json' % (geoserver_site_url, workspace_name, workspace_name)
+            response = get(rest_url, auth=auth)
+            response.raise_for_status()
+        except exceptions.HTTPError:
+            geo.create_featurestore(store_name='%s' % workspace_name, workspace='%s' % workspace_name,
+                                    db='%s' % pg_name,
+                                    host='%s' % pg_host,
+                                    pg_user='%s' % pg_user, pg_password='%s' % pg_pass)
+
+    # Function to retrieve spatial tables in PostgreSQL
+    def pg_connection_details(self, pg_host, pg_user, pg_pass, pg_name):
+        try:
+
+            connection = psycopg2.connect(host='%s' % pg_host, database='%s' % pg_name,
+                                          user='%s' % pg_user, password='%s' % pg_pass,
+                                          port=5432)
+            cursor = connection.cursor()
+            check_table = '''SELECT f_table_name from geometry_columns WHERE f_table_schema = 'public';'''
+            cursor.execute(check_table)
+            _tables = [row[0] for row in cursor.fetchall()]
+            cursor.close()
+            connection.close()
+        except psycopg2.OperationalError:
+            sys.exit(1)
+        return _tables
+
+    # Function to publish vector data as GeoServer layers
+    def publish_layer_stores(self, geoserver_site_url, username, user_pass, workspace_name, pg_host, pg_user, pg_pass,
+                             pg_name):
+        geo = Geoserver(geoserver_site_url, username='%s' % username, password='%s' % user_pass)
+        auth = HTTPBasicAuth('%s' % username, '%s' % user_pass)
+
+        pg_tables = self.pg_connection_details(pg_host, pg_user, pg_pass, pg_name)
+
+        for table in pg_tables:
+            try:
+                rest_url = '%s/rest/workspaces/%s/datastores/%s/featuretypes/%s.json' % (
+                    geoserver_site_url, workspace_name, workspace_name, table)
+                response = get(rest_url, auth=auth)
+                print(response.raise_for_status())
+                response.raise_for_status()
+            except exceptions.HTTPError:
+                geo.publish_featurestore(workspace='%s'
+                                                   % workspace_name, store_name='%s' % workspace_name,
+                                         pg_table='%s' % table)
+
+    # Function to update bounding box of layer
+    def recalculate_bbox(self, geo_site_url, username, user_pass, workspace_name, pg_host, pg_user, pg_pass, pg_name):
+        auth = HTTPBasicAuth('%s' % username, '%s' % user_pass)
+        headers = {"Content-type": "text/xml"}
+        xml_data = "<featureType><enabled>true</enabled></featureType>"
+        _tables = self.pg_connection_details(pg_host, pg_user, pg_pass, pg_name)
+        for table in _tables:
+            rest_url = '%s/rest/workspaces/%s/datastores/%s/featuretypes/%s?recalculate=nativebbox,latlonbbox' % (
+                geo_site_url, workspace_name, workspace_name, table)
+            response = put(rest_url, headers=headers, data=xml_data, auth=auth)
+            if response.status_code != 200:
+                return response.raise_for_status()
+
+    def geoserver_requests(self):
+        # Create geoserver workspace and set it as a default one
+        self.publish_workspace(self.default['GEOSERVER_INSTANCE_URL'], self.default['GEO_USER'],
+                               self.default['GEO_PASS'], self.default['GEO_WORKSPACE'])
+
+        # Create Database store connecting to PostgreSQL database
+        self.publish_store(self.default['GEOSERVER_INSTANCE_URL'], self.default['GEO_USER'], self.default['GEO_PASS'],
+                           self.default['GEO_WORKSPACE'], self.default['DATABASE_HOST'],
+                           self.default['DATABASE_USER'],
+                           self.default['DATABASE_PASSWORD'], self.default['DATABASE_NAME'])
+
+        # Publish layers from database to GeoServer with default styles
+
+        self.publish_layer_stores(self.default['GEOSERVER_INSTANCE_URL'], self.default['GEO_USER'],
+                                  self.default['GEO_PASS'], self.default['GEO_WORKSPACE'],
+                                  self.default['DATABASE_HOST'],
+                                  self.default['DATABASE_USER'],
+                                  self.default['DATABASE_PASSWORD'],
+                                  self.default['DATABASE_NAME'])
+
+        # Recalculate layer bounds in GeoServer layers in case the data has changed
+        self.recalculate_bbox(self.default['GEOSERVER_INSTANCE_URL'], self.default['GEO_USER'],
+                              self.default['GEO_PASS'], self.default['GEO_WORKSPACE'],
+                              self.default['DATABASE_HOST'],
+                              self.default['DATABASE_USER'],
+                              self.default['DATABASE_PASSWORD'], self.default['DATABASE_NAME'])
+
+
+if __name__ == '__main__':
+    importer = Importer()
+    importer.geoserver_requests()
