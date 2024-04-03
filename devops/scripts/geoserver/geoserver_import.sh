1d147ad002705a40c5bac44789d9d39c92adc5e1
spatialgeobyte <158478685+spatialgeobyte@users.noreply.github.com>
2024-04-03 13:42:22 +0200

    Fix GeoServer settings

--- devops/scripts/geoserver/geoserver_import.sh
+++ devops/scripts/geoserver/geoserver_import.sh
@@ -6,7 +6,7 @@ apt update;apt-get -y --no-install-recommends install python3-pip
 pip3 install geoserver-rest psycopg2-binary
 
 # Generate and populate services
-python3 geoserver_populate_services.py
+python3 /geoserver/geoserver_populate_services.py
 
 # Download images in your preferred format. If you need to adjust params, change default params in script
-python3 geoserver_generate_report.py
+python3 /geoserver/geoserver_generate_report.py
