cc127a300236727c2c73ba7b8c44b31b42305869
GerhardtB <gerhardtbotha5@gmail.com>
2024-04-10 11:10:59 +0200

    added hsi mapping fields to asset models

--- cwa/cwa_geodjango/cwa_geod/assets/models/network_meter.py
+++ cwa/cwa_geodjango/cwa_geod/assets/models/network_meter.py
@@ -5,6 +5,9 @@ from cwa_geod.core.constants import DEFAULT_SRID, NETWORK_METER_NAME
 
 class NetworkMeter(models.Model):
     gid = models.IntegerField(null=False, blank=False, unique=True, db_index=True)
+    hsi_id = models.IntegerField(null=False, blank=False, unique=True)
+    tag_name = models.CharField(null=False, blank=False, unique=True)
+
     geometry = models.PointField(
         spatial_index=True, null=False, blank=False, srid=DEFAULT_SRID
     )
--- cwa/cwa_geodjango/cwa_geod/assets/models/pressure_control_valve.py
+++ cwa/cwa_geodjango/cwa_geod/assets/models/pressure_control_valve.py
@@ -5,6 +5,8 @@ from cwa_geod.core.constants import DEFAULT_SRID, PRESSURE_CONTROL_VALVE_NAME
 
 class PressureControlValve(models.Model):
     gid = models.IntegerField(null=False, blank=False, unique=True, db_index=True)
+    hsi_id = models.IntegerField(null=False, blank=False, unique=True)
+    tag_name = models.CharField(null=False, blank=False, unique=True)
     geometry = models.PointField(
         spatial_index=True, null=False, blank=False, srid=DEFAULT_SRID
     )
