bc408088e4a580b4a1945b6ec76dbe683d10959e
Zaheer <{ID}+{username}@users.noreply.github.com>
2024-04-16 08:34:09 +0200

    fix to mains model order

--- cwa/cwa_geodjango/cwa_geod/assets/models/trunk_main.py
+++ cwa/cwa_geodjango/cwa_geod/assets/models/trunk_main.py
@@ -13,7 +13,7 @@ class TrunkMain(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
 
     class Meta:
-        ordering = ["dmas"]
+        ordering = ["dmas_id"]
 
     class AssetMeta:
         asset_name = TRUNK_MAIN__NAME
