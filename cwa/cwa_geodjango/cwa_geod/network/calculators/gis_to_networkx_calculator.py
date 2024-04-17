bec1a10aac3049fcd0eba7a2595570a6d3c99d13
marthinus-sandtech <mvanderwalt@sandtech.com>
2024-04-17 11:16:42 +0200

    graph methods updated
    refactored into methods, adjusted approach to dictionary of build triads. Triads are node-edge-node sets: {
    edge_gid: edge_gid_value,
    src: source_node_id,
    dst: destination_node_id
    }.

--- cwa/cwa_geodjango/cwa_geod/network/calculators/gis_to_networkx_calculator.py
+++ cwa/cwa_geodjango/cwa_geod/network/calculators/gis_to_networkx_calculator.py
@@ -1,6 +1,7 @@
 import networkx as nx
 from networkx import Graph
 from cleanwater.calculators import GisToGraphCalculator
+import matplotlib.pyplot as plt
 import bdb
 
 class GisToNxCalculator(GisToGraphCalculator):
@@ -13,6 +14,8 @@ class GisToNxCalculator(GisToGraphCalculator):
         self.all_base_pipes = []
         self.all_nodes_ordered = []
         super().__init__(self.config)
+
+
 
     def extract_assets_and_junctions(self):
         """Extract assets and junctions from the input data"""
@@ -47,7 +50,7 @@ class GisToNxCalculator(GisToGraphCalculator):
 
     def build_adjacency_dict(self, junctions_dict):
         """Build an adjacency dictionary based on junctions"""
-        adjacency_dict = {}
+        edges_dict = {}
 
         for feature, info in junctions_dict.items():
             node_id = info['node_id']
@@ -55,53 +58,95 @@ class GisToNxCalculator(GisToGraphCalculator):
 
             if pipe_gids:
                 for pipe_gid in pipe_gids:
-                    if pipe_gid not in adjacency_dict:
-                        adjacency_dict[pipe_gid] = []
+                    if pipe_gid not in edges_dict:
+                        edges_dict[pipe_gid] = []
 
-                    if node_id not in adjacency_dict[pipe_gid]:
-                        adjacency_dict[pipe_gid].append(node_id)
+                    if node_id not in edges_dict[pipe_gid]:
+                        edges_dict[pipe_gid].append(node_id)
             else:
-                print(f"No pipe_gids found for node_id: {node_id}. Skipping...")
+                # print(f"No pipe_gids found for node_id: {node_id}. Skipping...")
+                continue
 
+        # transform edge dictionary into useful format
+        adjacency_dict = {}
+        skip_count = 0
+
+        for edge, vertices in edges_dict.items():
+
+            if len(vertices) != 2:
+                skip_count += 1
+                continue
+
+            src, dst = vertices
+
+            # Check if either src or dst is missing and add a placeholder
+            if src is None:
+                src = 'None'
+            if dst is None:
+                dst = 'None'
+
+            adjacency_dict[edge] = {'src': src, 'dst': dst}
+        print("skipped ", skip_count, " edges")
         return adjacency_dict
 
     def build_graph_from_adjacency(self, adjacency_dict):
         """Build a NetworkX graph from the adjacency dictionary"""
-        for pipe_gid, node_ids in adjacency_dict.items():
-            for node_id in node_ids:
-                if node_id not in self.G.nodes:
-                    self.G.add_node(node_id)
+        # Iterate through the edge dictionary and add nodes and edges to the graph
+        for edge_id, edge_info in adjacency_dict.items():
+            src = edge_info['src']
+            dst = edge_info['dst']
 
-                for next_node_id in node_ids[node_ids.index(node_id) + 1:]:
-                    self.G.add_edge(node_id, next_node_id, pipe_gid=pipe_gid)
+            # Add nodes and edges to the graph
+            self.G.add_edge(src, dst, edge_id=edge_id)
 
-        # Print nodes and edges to verify
-        print("Nodes:", self.G.nodes())
-        print("Edges:", self.G.edges(data=True))
+
 
     def integrate_coincident_nodes(self, asset_dict, junctions_dict):
         """Integrate coincident nodes from asset_dict into the graph"""
+        int_count = 0
+        non_count = 0
         for feature, asset_info in asset_dict.items():
             asset_pipe_gids = asset_info['pipe_gids']
             asset_node_id = asset_info['node_id']
 
             for junction_info in junctions_dict.values():
                 junction_pipe_gids = junction_info['pipe_gids']
-                junction_node_id = junction_info['node_id']
+                # junction_node_id = junction_info['node_id']
 
-                matching_pipe_gids = set(asset_pipe_gids) & set(junction_pipe_gids)
+                if asset_pipe_gids is not None and junction_pipe_gids is not None:
+                    matching_pipe_gids = set(asset_pipe_gids) & set(junction_pipe_gids)
+                    int_count += 1
+                else:
+                    non_count += 1
+                    continue
 
                 if matching_pipe_gids:
                     if asset_node_id not in self.G.nodes:
                         self.G.add_node(asset_node_id)
 
-                    for neighbor_pipe_gid in matching_pipe_gids:
-                        if neighbor_pipe_gid in self.G:
-                            neighbor_node_id = self.G.nodes[neighbor_pipe_gid]['node_id']
-                            self.G.add_edge(asset_node_id, neighbor_node_id, pipe_gid=neighbor_pipe_gid)
-
+                    for neighbour_pipe_gid in matching_pipe_gids:
+                        if neighbour_pipe_gid in self.G:
+                            neighbor_node_id = self.G.nodes[neighbour_pipe_gid]['node_id']
+                            self.G.add_edge(asset_node_id, neighbor_node_id, pipe_gid=neighbour_pipe_gid)
+        print("nodes merged: ", int_count)
+        print("nodes skipped: ", non_count)
         # Remove duplicate edges to ensure the graph is consistent
         self.G = nx.Graph(self.G)
+
+    def remove_isolated_nodes_and_plot(self):
+        filename = 'Graph.png'
+        components = list(nx.connected_components(self.G))
+        isolated_nodes = [node for component in components for node in component if len(component) == 1]
+
+        self.G.remove_nodes_from(isolated_nodes)
+
+        # Plot and save the graph
+        nx.draw(self.G, with_labels=False, node_color='black', node_size=20, edge_color='red', linewidths=1,
+                font_size=15)
+        plt.savefig(filename)
+        plt.close()
+
+        print(f"Graph saved as {filename} in the current directory.")
 
     def create_nx_graph(self):
         """Create the NetworkX graph from the geospatial network"""
@@ -110,4 +155,8 @@ class GisToNxCalculator(GisToGraphCalculator):
 
         self.build_graph_from_adjacency(adjacency_dict)
         self.integrate_coincident_nodes(asset_dict, junctions_dict)
+        self.remove_isolated_nodes_and_plot()
 
+        # Print nodes and edges to verify
+        print("Nodes:", len(self.G.nodes()))
+        print("Edges:", len(self.G.edges(data=True)))
