708db8818ddc6d569e8a7e3dc2126082e3dfc5b0
gerald-eai <gerald@explore.ai>
2024-04-25 15:51:49 +0200

    Updated implemented pagination class of gis app to inherit from the Base Pagination file

--- api/api_drf/cw_network/gis/pagination.py
+++ api/api_drf/cw_network/gis/pagination.py
@@ -1,16 +1,7 @@
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
+from config.pagination.base_pagination import BasePagination
 
-class StandardResultsSetPagination(PageNumberPagination):
+class StandardResultsSetPagination(BasePagination):
     # :TODO Extend Pagination class to support custom page size
-    page_size = 100
-    page_size_query_param = 'page_size'
-    max_page_size = 100
-    
-    def get_paginated_response(self, data):
-        return Response({
-            'next': self.get_next_link(),
-            'previous': self.get_previous_link(),
-            'count': self.page.paginator.count,
-            'results': data
-        })
+    pass 
