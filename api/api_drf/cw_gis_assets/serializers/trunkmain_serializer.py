894e260e1c62e176887e0f4b9f596952c3c8874c
gerald-eai <gerald@explore.ai>
2024-05-02 16:33:37 +0200

    Created initial Query Set for Trunk Mains GeoJSONs

--- api/api_drf/cw_gis_assets/serializers/trunkmain_serializer.py
+++ api/api_drf/cw_gis_assets/serializers/trunkmain_serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from cwageodjango.assets.models.trunk_main import TrunkMain
+import json
 
 
 class TrunkMainSerializer(serializers.ModelSerializer):
@@ -7,3 +8,31 @@ class TrunkMainSerializer(serializers.ModelSerializer):
         model = TrunkMain
         fields = ["gid", "geometry", "dmas", "modified_at", "created_at"]
         read_only_fields = ("id",)
+
+
+class TrunkMainGeoJsonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TrunkMain
+        fields = ["gid", "geometry"]
+        read_only_fields = ("id",)
+
+    def to_representation(self, instance, srid):
+        srid: int | None = srid
+
+        # geo_data: dict = {
+        #     "type": "FeatureCollection",
+        #     "crs": {"type": "name", "properties": {"name": f"EPSG:{srid}"}},
+        #     "features": list(qs),
+        # }
+        # return json.dumps(geo_data)
+
+        geo_data: dict = {
+            "type": "Feature",
+            # "crs": {"type": "name", "properties": {"name": f"EPSG:{srid}"}},
+            "properties": {
+                "gid": instance.gid
+            },
+            "geometry": json.loads(instance.geometry),
+        }
+
+        return geo_data
