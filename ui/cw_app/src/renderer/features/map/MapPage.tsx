a1d24604ad8486673b43c65b12340e63483396a8
gerald-eai <gerald@explore.ai>
2024-05-08 20:09:17 +0200

    Updated routes, and themes. Setup DeckGL installs for future implementation.

--- ui/cw_app/src/renderer/features/map/MapPage.tsx
+++ ui/cw_app/src/renderer/features/map/MapPage.tsx
@@ -1,10 +1,24 @@
-// Our Map Page details exists here 
+// Our Map Page details exists here
+import DeckGL from '@deck.gl/react';
+import { MapViewState } from '@deck.gl/core';
 
+
+const INITIAL_VIEW_STATE: MapViewState = {
+    longitude: -122.4,
+    latitude: 37.8,
+    zoom: 15,
+    bearing: 0,
+    pitch: 30,
+};
 
 export default function MapPage() {
     return (
-        <div>
-            <h1>Map Page</h1>
+        <div style={{ border: '1px solid black' }}>
+            <DeckGL
+                initialViewState={INITIAL_VIEW_STATE}
+                controller={{ scrollZoom: true }}
+            >
+            </DeckGL>
         </div>
-    )
+    );
 }
