000cf8a6351e77ace12b52998bb43bbdf7cf0a33
spatialgeobyte <158478685+spatialgeobyte@users.noreply.github.com>
2024-05-10 15:24:43 +0200

    fix generating single outputs

--- geoserver/geoserver_generate_report.py
+++ geoserver/geoserver_generate_report.py
@@ -11,16 +11,16 @@ class Downloader:
     def __init__(self):
         self.geoserver_config = GeoServerConfig()
         self.geoserver_auth = self.geoserver_config.geoserver_auth
+        self.url_set = set()
 
     # Function to download and save images from WMS endpoints
     def generate_output(self, url, file_path):
         """"
         Helper function to save the output to a file
         """
-        url_set = set()  # Create a set to store URLs
         auth = self.geoserver_auth.auth
 
-        if url in url_set:  # Check if the URL already exists
+        if url in self.url_set:  # Check if the URL already exists
             print("Image already downloaded, skipping:", url)
         else:
             try:
@@ -29,8 +29,7 @@ class Downloader:
                 with open(file_path, 'wb') as file:
                     file.write(response.content)
                 file.close()
-                print("Image saved successfully:", file_path)
-                url_set.add(url)  # Add the URL to the set
+                self.url_set.add(url)  # Add the URL to the set
             except exceptions.RequestException as e:
                 print("Error downloading image:", e)
 
@@ -52,36 +51,33 @@ class Downloader:
         if pg_tables:
             for table_name, bbox in pg_tables:
                 # Check if the store already exists
-                try:
-                    rest_url = '%s/rest/workspaces/%s/datastores/%s.json' % (
-                        geoserver_site_url, workspace_name, workspace_name)
-                    response = get(rest_url, auth=auth)
-                    response.raise_for_status()
-                    if response.status_code == 200:
-                        layer_url = "%s/rest/workspaces/%s/datastores/%s/featuretypes/%s.json" % (
-                            geoserver_site_url, self.geoserver_config.default['GEO_WORKSPACE'],
-                            self.geoserver_config.default['GEO_WORKSPACE'], table_name)
-                        layer_response = get(layer_url, auth=auth)
-                        layer_response.raise_for_status()
-                        if layer_response.status_code == 200:
-                            get_map_url = '%s/%s/wms?service=WMS&version=1.1.0&request=GetMap&layers=%s:%s&bbox=%s,%s,%s,%s&width=%s&height=%s&srs=EPSG:%s&styles=&format=%s' % (
-                                geoserver_site_url, workspace_name, workspace_name, table_name, bbox[0], bbox[1],
-                                bbox[2],
-                                bbox[3],
-                                self.geoserver_config.default['HEIGHT'], self.geoserver_config.default['WIDTH'],
-                                bbox[4],
-                                self.geoserver_config.default['FORMAT'])
-                            image_extension = self.geoserver_config.default['FORMAT'].split('/')[-1]
-                            base_path = join("/geoserver_scripts", "output")
-                            if not exists(base_path):
-                                makedirs(base_path)
-                            time_stamp = time.strftime("%Y%m%d-%H%M%S")
-                            report_image = f"{table_name}_{time_stamp}.{image_extension}"
-                            file_path = join(base_path, report_image)
-                            # Download and save the image
-                            self.generate_output(get_map_url, file_path)
-                except exceptions.HTTPError:
-                    print("Store is not defined")
+                rest_url = '%s/rest/workspaces/%s/datastores/%s.json' % (
+                    geoserver_site_url, workspace_name, workspace_name)
+                response = get(rest_url, auth=auth)
+                response.raise_for_status()
+                if response.status_code == 200:
+                    layer_url = "%s/rest/workspaces/%s/datastores/%s/featuretypes/%s.json" % (
+                        geoserver_site_url, self.geoserver_config.default['GEO_WORKSPACE'],
+                        self.geoserver_config.default['GEO_WORKSPACE'], table_name)
+                    layer_response = get(layer_url, auth=auth)
+                    layer_response.raise_for_status()
+                    if layer_response.status_code == 200:
+                        get_map_url = '%s/%s/wms?service=WMS&version=1.1.0&request=GetMap&layers=%s:%s&bbox=%s,%s,%s,%s&width=%s&height=%s&srs=EPSG:%s&styles=&format=%s' % (
+                            geoserver_site_url, workspace_name, workspace_name, table_name, bbox[0], bbox[1],
+                            bbox[2],
+                            bbox[3],
+                            self.geoserver_config.default['HEIGHT'], self.geoserver_config.default['WIDTH'],
+                            bbox[4],
+                            self.geoserver_config.default['FORMAT'])
+                        image_extension = self.geoserver_config.default['FORMAT'].split('/')[-1]
+                        base_path = join("/geoserver_scripts", "output")
+                        if not exists(base_path):
+                            makedirs(base_path)
+                        time_stamp = time.strftime("%Y%m%d-%H%M%S")
+                        report_image = f"{table_name}_{time_stamp}.{image_extension}"
+                        file_path = join(base_path, report_image)
+                        # Download and save the image
+                        self.generate_output(get_map_url, file_path)
 
     def get_layer_snapshot_per_dma(self, geoserver_site_url, workspace_name):
         """
@@ -102,44 +98,39 @@ class Downloader:
             for table_name, geom_type in spatial_tables:
 
                 # Check if the store already exists
-                try:
-                    rest_url = '%s/rest/workspaces/%s/datastores/%s.json' % (
-                        geoserver_site_url, workspace_name, workspace_name)
-                    response = get(rest_url, auth=auth)
-                    response.raise_for_status()
-                    if response.status_code == 200:
-                        layer_url = "%s/rest/workspaces/%s/datastores/%s/featuretypes/%s.json" % (
-                            geoserver_site_url, self.geoserver_config.default['GEO_WORKSPACE'],
-                            self.geoserver_config.default['GEO_WORKSPACE'], table_name)
-                        layer_response = get(layer_url, auth=auth)
-                        layer_response.raise_for_status()
-                        if layer_response.status_code == 200:
-                            dma_region_codes = ['ZWAL4801', 'ZCHESS12', 'ZCHIPO01']
-                            dma_codes_list = db_conn.pg_dma_extent_details(dma_region_codes)
-                            for code, coordinates in dma_codes_list:
-                                min_x, min_y, max_x, max_y, srid = coordinates
-                                # TODO fix bbox assignment
-                                get_map_url = '%s/%s/wms?service=WMS&version=1.1.0&request=GetMap&layers=%s:%s&bbox=%s,%s,%s,%s&width=%s&height=%s&srs=EPSG:%s&styles=&format=%s' % (
-                                    geoserver_site_url, workspace_name, workspace_name, table_name, min_x, min_y,
-                                    max_x,
-                                    max_y,
-                                    self.geoserver_config.default['HEIGHT'], self.geoserver_config.default['WIDTH'],
-                                    srid,
-                                    self.geoserver_config.default['FORMAT'])
+                rest_url = '%s/rest/workspaces/%s/datastores/%s.json' % (
+                    geoserver_site_url, workspace_name, workspace_name)
+                response = get(rest_url, auth=auth)
+                response.raise_for_status()
+                if response.status_code == 200:
+                    layer_url = "%s/rest/workspaces/%s/datastores/%s/featuretypes/%s.json" % (
+                        geoserver_site_url, self.geoserver_config.default['GEO_WORKSPACE'],
+                        self.geoserver_config.default['GEO_WORKSPACE'], table_name)
+                    layer_response = get(layer_url, auth=auth)
+                    layer_response.raise_for_status()
+                    if layer_response.status_code == 200:
+                        dma_region_codes = ['ZWAL4801', 'ZCHESS12', 'ZCHIPO01']
+                        dma_codes_list = db_conn.pg_dma_extent_details(dma_region_codes)
+                        for code, coordinates in dma_codes_list:
+                            min_x, min_y, max_x, max_y, srid = coordinates
+                            # TODO fix bbox assignment
+                            get_map_url = '%s/%s/wms?service=WMS&version=1.1.0&request=GetMap&layers=%s:%s&bbox=%s,%s,%s,%s&width=%s&height=%s&srs=EPSG:%s&styles=&format=%s' % (
+                                geoserver_site_url, workspace_name, workspace_name, table_name, min_x, min_y,
+                                max_x,
+                                max_y,
+                                self.geoserver_config.default['HEIGHT'], self.geoserver_config.default['WIDTH'],
+                                srid,
+                                self.geoserver_config.default['FORMAT'])
 
-                                print(get_map_url)
-                                image_ext = self.geoserver_config.default['FORMAT'].split('/')[-1]
-                                base_path = join("/geoserver_scripts", "output")
-                                if not exists(base_path):
-                                    makedirs(base_path)
-                                time_stamp = time.strftime("%Y%m%d-%H%M%S")
-                                report_image = f"{table_name}_{code}_{time_stamp}.{image_ext}"
-                                file_path = join(base_path, report_image)
-                                # Download and save the image
-                                self.generate_output(get_map_url, file_path)
-
-                except exceptions.HTTPError:
-                    print("checking if layer is published")
+                            image_ext = self.geoserver_config.default['FORMAT'].split('/')[-1]
+                            base_path = join("/geoserver_scripts", "output")
+                            if not exists(base_path):
+                                makedirs(base_path)
+                            time_stamp = time.strftime("%Y%m%d-%H%M%S")
+                            report_image = f"{table_name}_{code}_{time_stamp}.{image_ext}"
+                            file_path = join(base_path, report_image)
+                            # Download and save the image
+                            self.generate_output(get_map_url, file_path)
 
     def geoserver_get_map(self):
 
