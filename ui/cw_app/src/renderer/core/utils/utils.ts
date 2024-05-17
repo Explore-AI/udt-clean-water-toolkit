8cadfcf8ccca73bc276e7818150f8e368b27d657
gerald-eai <gerald@explore.ai>
2024-05-17 16:53:06 +0200

    Scaffolded Search box functions for further implementation.

---
+++ ui/cw_app/src/renderer/core/utils/utils.ts
@@ -0,0 +1,11 @@
+// add our utility functions here 
+
+
+export const getCoordinateData = () => {}
+
+export const getAddressData = () => {}
+
+export const getAssetData = () => {
+    // after receiving the asset id, whether it be gisid, dma, or fmz then make a request to the 
+    // geoserver api to collect the data
+} 
