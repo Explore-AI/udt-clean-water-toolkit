1f7ac4e734f172594be9499825cc0c0d5cd2f630
Zaheer <{ID}+{username}@users.noreply.github.com>
2024-05-19 20:56:27 +0200

    added neo4j browser

--- ui/cw_app/src/renderer/graph/components/GraphPage.tsx
+++ ui/cw_app/src/renderer/graph/components/GraphPage.tsx
@@ -3,7 +3,7 @@ import BaseLayout from '../../core/components/BaseLayout';
 export default function GraphPage() {
     return (
         <BaseLayout>
-            <iframe src="http://localhost:8080/" title="neo4j browser" frameborder="0" style={{overflow:'hidden', height:'100vh', width:'100%'}}></iframe>
+            <iframe src="http://localhost:7475/" title="neo4j browser" frameBorder="0" style={{overflow:'hidden', height:'100vh', width:'100%'}}></iframe>
         </BaseLayout>
     );
 }
