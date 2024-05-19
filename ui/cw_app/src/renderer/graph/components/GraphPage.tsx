b023d13a3ef91839671fe835585f00e3b211b2f4
Zaheer <{ID}+{username}@users.noreply.github.com>
2024-05-19 10:19:55 +0200

    added neo4j iframe

--- ui/cw_app/src/renderer/graph/components/GraphPage.tsx
+++ ui/cw_app/src/renderer/graph/components/GraphPage.tsx
@@ -3,7 +3,7 @@ import BaseLayout from '../../core/components/BaseLayout';
 export default function GraphPage() {
     return (
         <BaseLayout>
-          fdsfds
+            <iframe src="http://localhost:8080/" title="neo4j browser" frameborder="0" style={{overflow:'hidden', height:'100vh', width:'100%'}}></iframe>
         </BaseLayout>
     );
 }
