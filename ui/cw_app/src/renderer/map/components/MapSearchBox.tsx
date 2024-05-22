3ff7954f75d1b0e95fed68ae2252fc0fb9947c75
Zaheer <{ID}+{username}@users.noreply.github.com>
2024-05-22 17:49:45 +0200

    search refactor

--- ui/cw_app/src/renderer/map/components/MapSearchBox.tsx
+++ ui/cw_app/src/renderer/map/components/MapSearchBox.tsx
@@ -1,9 +1,9 @@
 // this is our common search input that will be used in a lot of other components
-import React, { useState, SetStateAction } from 'react';
-import { TextInput, CloseButton } from '@mantine/core';
 import styles from '../css/MapSearchBox.module.css';
+import axios from 'axios';
+import { TextInput, CloseButton } from '@mantine/core';
 import { IconSearch, IconAlertTriangle } from '@tabler/icons-react';
-import { validateInput, getNominatimData } from '../utils/utils';
+import { validateInput, getNominatimData } from '../../core/utils/utils';
 
 
 
@@ -18,8 +18,41 @@ export default function MapSearchBox() {
         />
     );
 
-    const onSearch = (e) => {
-        console.log(e.target.value)
+    const getLocation = async ({ payload, type }: GoToProps) => {
+        const addressProps = {
+            urlExtension: 'search',
+            params: {
+                q: payload as string,
+                format: 'jsonv2',
+                limit: 1,
+                addressdetails: 1,
+            },
+        };
+        const addressData = await getNominatimData(addressProps);
+        return addressData;
+    };
+
+    const onSearch = async (e) => {
+        const validInput = validateInput(e.target.value);
+        if (!validInput?.validated) {
+            //setHasError(true);
+            return;
+        } else {
+            //setHasError(false);
+            // set the go to location
+            if (validInput.type == 'gps') {
+                updateGoToCoords({
+                    latitude: validInput.payload[0] as number,
+                    longitude: validInput.payload[1] as number,
+                });
+            } else {
+                const goToLocation = await getLocation(validInput);
+                updateGoToCoords({
+                    latitude: goToLocation.lat,
+                    longitude: goToLocation.lon,
+                });
+            }
+        }
     }
 
     return (
