3a1de5568fc9fcbb4e64333b8d7d8714f0befecf
GerhardtB <gerhardtbotha5@gmail.com>
2024-05-22 13:36:40 +0200

    update

--- cwa/cwa_geodjango/cwageodjango/network/controllers/acoustic_logger_coverage.py
+++ cwa/cwa_geodjango/cwageodjango/network/controllers/acoustic_logger_coverage.py
@@ -1,7 +1,7 @@
 from cwageodjango.network.models import *
 from neomodel import db
 import csv
-import numpy as np
+import pandas as pd
 
 class AcousticLoggerCoverage():
     def __init__(self, config):
@@ -34,10 +34,14 @@ class AcousticLoggerCoverage():
                   'alloyed_polyvinyl_chloride':70,
                   'biaxial_polyvinyl_chloride':70,
                   'galvanized_steel':150}
-    def initialize_csv(self):
+        
+    def initialise_csv(self):
         with open(self.config.outputfile, mode='w', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow(['logger_node_key', 'pipe_id', 'coverage_length'])
+            writer.writerow(['logger_node_key','pipe_id','pipe_material',
+                             'pipe_length', 'coverage_length','pipe_wkt',
+                             'start_node_key','start_node_coords','end_node_key', 
+                             'end_node_coords'])
 
     def get_connected(self, node_key, max_level):
         cypher_query = f"""
@@ -67,7 +71,6 @@ class AcousticLoggerCoverage():
         results, _ = db.cypher_query(cypher_query)
         return results
     
-
     def update_edge_attributes(self, start_node_key, end_node_key, logger, coverage_len):
             query = f"""
             MATCH (n {{node_key: '{start_node_key}'}})-[r]-(m {{node_key: '{end_node_key}'}})
@@ -75,89 +78,111 @@ class AcousticLoggerCoverage():
                 CASE WHEN r.coveredbyLogger IS NOT NULL THEN r.coveredbyLogger + '|' + '{logger}' ELSE '{logger}' END AS newCoveredByLogger,
                 CASE WHEN r.coverageLen IS NOT NULL THEN r.coverageLen + '|' + '{coverage_len}' ELSE '{coverage_len}' END AS newCoverageLen,
                 n.node_key AS startNodeKey,
-                m.node_key AS endNodeKey
+                n.coords_27700 AS startNodeCoords,
+                m.node_key AS endNodeKey,
+                m.coords_27700 as endNodeCoords, 
+                r.material AS PipeMaterial,
+                r.segment_length AS PipeLength,
+                r.segment_wkt AS PipeWkt
             SET r.coveredbyLogger = newCoveredByLogger,
                 r.coverageLen = newCoverageLen
-            RETURN id(r) AS pipe_id, '{coverage_len}' AS coverage_length, startNodeKey, endNodeKey
+            RETURN id(r) AS pipe_id, '{coverage_len}' AS coverage_length, startNodeKey, startNodeCoords, endNodeKey, endNodeCoords, PipeMaterial, PipeLength, PipeWkt 
             """
             results, _ = db.cypher_query(query)
 
-            # Log to CSV
             with open(self.config.outputfile, mode='a', newline='') as file:
                 writer = csv.writer(file)
                 for result in results:
                     pipe_id = result[0]
                     coverage_length = result[1]
                     start_node_key = result[2]
-                    end_node_key = result[3]
-                    writer.writerow([logger, pipe_id, coverage_length, start_node_key, end_node_key])
+                    start_node_coords = result[3]
+                    end_node_key = result[4]
+                    end_node_coords = result[5]
+                    pipe_material = result[6]
+                    pipe_length = result[7]
+                    pipe_wkt = result[8]
+                    writer.writerow([logger, pipe_id, pipe_material, pipe_length, coverage_length, pipe_wkt, start_node_key, start_node_coords,
+                                     end_node_key, end_node_coords])
 
     def check_for_pipe_end(self, node_key):
         query = f"MATCH (n {{node_key : '{node_key}'}}) RETURN 'PipeEnd' IN labels(n) AS is_pipe_end"
         results, _ = db.cypher_query(query)
-        is_pipe_end_str = results[0][0]  # This might be a string "True" or "False"
-        #is_pipe_end = is_pipe_end_str.lower() == "true"  # Convert to boolean
+        is_pipe_end_str = results[0][0]
         return is_pipe_end_str
+    
+    def convert_remaining_distance(self, remaining_distance, current_material, next_material):
+        current_material_distance = self.detection_dist.get(current_material)
+        next_material_distance = self.detection_dist.get(next_material)
+        converted_distance = (remaining_distance / current_material_distance) * next_material_distance
+        return converted_distance
 
-    def process_connected_edges(self, node_key, original_remaining_distance, remaining_distance, processed_edges, logger_key):
+    def process_connected_edges(self, node_key, remaining_distance, processed_edges, processed_nodes, pipe_material, logger_key, is_initial):
         if self.check_for_pipe_end(node_key=node_key):
-            print("Node is labeled as PipeEnd. Skipping processing.")
-            return    
+            return
+
         while remaining_distance > 0:
-            print(f'DISTANCE {remaining_distance}')
-            processed_nodes = set()
             processed_nodes.add(node_key)
             next_edges = self.get_next_edges(node_key, processed_edges)
-            print(f'Getting next connected edges at {node_key}.')
+
+            if not is_initial and len(next_edges) > 1:
+                per_edge_distance = remaining_distance / len(next_edges)
+            else:
+                per_edge_distance = remaining_distance
+
             for edge in next_edges:
+                if 'edge_distance' not in locals() or edge_distance == 0:
+                    edge_distance = per_edge_distance
+                
+                edge_distance = self.convert_remaining_distance(edge_distance, pipe_material, edge[1].get('material'))
                 start_node_key = edge[1]._start_node.get('node_key')
                 end_node_key = edge[1]._end_node.get('node_key')
-
                 pipe_id = edge[1]._id
                 pipe_material = edge[1].get('material')
                 pipe_length = edge[1].get('segment_length')
-                travel_distance = self.detection_dist.get(pipe_material)
 
-                if pipe_length <= remaining_distance:
-                    remaining_distance -= pipe_length  # Update remaining_distance
-                    print(f'Next pipe {pipe_id} covered', 'remaining', remaining_distance)
+                if pipe_length > edge_distance:
+                    covered_distance = edge_distance
+                    remaining_distance -= edge_distance
+                    self.update_edge_attributes(start_node_key, 
+                                                end_node_key,
+                                                logger_key,
+                                                covered_distance)
+                    
+                    processed_edges.add(pipe_id)
+                    edge_distance = 0
+                    node_key = end_node_key if start_node_key in processed_nodes else start_node_key
+
+                    if self.check_for_pipe_end(node_key=node_key):
+                        edge_distance = 0
+                        break
+
+                elif pipe_length <= edge_distance:
+                    edge_distance -= pipe_length
+                    remaining_distance -= pipe_length  
 
                     self.update_edge_attributes(start_node_key, 
                                                 end_node_key,
                                                 logger_key,
                                                 pipe_length)
 
-                    processed_edges.add(pipe_id)  # Add pipe_id to processed_edges
+                    processed_edges.add(pipe_id)  
+
                     if remaining_distance > 0:
-
-                        if start_node_key in processed_nodes:
-                            node_key = end_node_key  # Update node_key for next iteration
-                        else:
-                            node_key = start_node_key
-                        # Check if the current node is a "PipeEnd" node before proceeding
-                        if not self.check_for_pipe_end(node_key=node_key):
-                            # Proceed with the next connected edges if the current node is not a "PipeEnd" node
-                            break
-                        else:
-                            print("Node is labeled as PipeEnd. Skipping processing.")
-                            continue  # Exit the loop and the function
-                        
-                elif pipe_length > remaining_distance:
-                    covered_distance = pipe_length - remaining_distance
-
-                    self.update_edge_attributes(start_node_key, 
-                                                end_node_key,
-                                                logger_key,
-                                                remaining_distance)
+                        node_key = end_node_key if start_node_key in processed_nodes else start_node_key
                     
-                    print(f'Whole next pipe {pipe_id} is not covered.', covered_distance, 'of', pipe_length, 
-                        'is remaining.')
-                    remaining_distance = 0  # Set remaining_distance to 0 since the pipe is longer than remaining distance
-                    break  # Exit the loop to get next connected edges
-                processed_edges.add(pipe_id)
-            else:
-                break  # Break out of while loop if remaining_distance is 0 or no more edges to process
+                        if self.check_for_pipe_end(node_key=node_key):
+                            remaining_distance -= edge_distance 
+                            edge_distance = 0
+                            continue
 
+                    if edge_distance > 0 and remaining_distance > 0:
+                        is_initial = False
+                        self.process_connected_edges(node_key, edge_distance, processed_edges, processed_nodes, pipe_material, logger_key, is_initial)
+                    else:
+                        break
+            else:
+                break
 
     def query_graph_dma(self, dma):
         """
@@ -166,29 +191,21 @@ class AcousticLoggerCoverage():
         Parameters:
             dma (str): DMA code to filter loggers.
 
-        Yields:
+        Returns:
             results: Result object containing query results.
 
         """    
-
-        results, m = db.cypher_query(f"MATCH (n) WHERE n.acoustic_logger IS NOT NULL and n.dmas contains '{dma[-1]}' RETURN n limit 10")
+        results, m = db.cypher_query(f"MATCH (n) WHERE n.acoustic_logger IS NOT NULL and n.dmas contains '{dma[-1]}' RETURN n")
         return results
 
     def process_logger(self, loggers):
         for node in loggers:
-
             key = node[0].get('node_key')
-            
-
             gid = node[0].get('asset_gids')[0]
-
-            print(f'........Starting with Logger: {key}...........')
-
-            initial_max_level = 1
-            max_detection = 300
 
             connected_edges = self.get_next_edges(key, set())
             processed_edges = set()
+            processed_nodes = set()
 
             for edge in connected_edges:
                 start_node_key = edge[1]._start_node.get('node_key')
@@ -201,35 +218,89 @@ class AcousticLoggerCoverage():
 
                 if pipe_length <= travel_distance:
                     remaining_distance = travel_distance - pipe_length
-                    print(f'Pipe {pipe_id} covered', 'remaining', remaining_distance)
-                    self.update_edge_attributes(start_node_key, 
-                                                end_node_key,
-                                                key,
-                                                pipe_length)
-                    processed_edges.add(pipe_id)  # Add pipe_id to processed_edges
+                    self.update_edge_attributes(start_node_key, end_node_key, key, pipe_length)
+                    processed_edges.add(pipe_id)
+                    processed_nodes.add(key)
+
                     if remaining_distance > 0:
-                        self.process_connected_edges(end_node_key, remaining_distance, remaining_distance, processed_edges, logger_key=key)
+                        node_key = end_node_key if start_node_key in processed_nodes else start_node_key
+                        self.process_connected_edges(node_key, 
+                                                     remaining_distance, 
+                                                     processed_edges,
+                                                     processed_nodes,
+                                                     pipe_material,
+                                                     logger_key=key,
+                                                     is_initial=False)
                 elif pipe_length > travel_distance:
                     covered_distance = pipe_length - travel_distance
-                    print(f'Whole pipe {pipe_id} is not covered.', covered_distance, 'of', pipe_length, 'is remaining.')
-                    self.update_edge_attributes(start_node_key, 
-                                                end_node_key,
-                                                key,
-                                                travel_distance)
+                    self.update_edge_attributes(start_node_key, end_node_key, key, travel_distance)
                     continue
-            print(f'........Finshed with Logger: {key}........')
 
+    def query_total_pipe_lengths_dma(self, dma):
+        results, m = db.cypher_query(f"MATCH (n)-[r]-(m) WHERE n.dmas contains '{dma[-1]}' RETURN sum(r.segment_length);")
+        return results
+
+    def summary_statistics(self, outputfile):
+        df = pd.read_csv(outputfile, header=[0])
+        dma = self.config.dma_codes[-1]
+        total_pipe_lengths = self.query_total_pipe_lengths_dma(self.config.dma_codes)[0][0]
+        total_pipe_lengths_covered = 0
+        for pipe in df.pipe_id.unique():
+            dftmp = df.loc[df['pipe_id'] == pipe]
+            dftmp = dftmp.drop_duplicates()
+            if len(dftmp) > 1:
+                pipe_len = dftmp['pipe_length'].iloc[0]
+                pipe_cov = dftmp['coverage_length'].sum()
+                total_cov = pipe_cov if pipe_cov < pipe_len else pipe_len
+                total_pipe_lengths_covered += total_cov
+            else:
+                total_pipe_lengths_covered += dftmp['coverage_length'].iloc[0]
+        precentage_coverage = (total_pipe_lengths_covered/total_pipe_lengths)*100
+        summary_file = str('_summary.csv').join(self.config.outputfile.split('.csv'))
+        with open(summary_file, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['dma','total_pipe_lengths','total_pipe_lenghts_covered',
+                             'percentage_covered'])
+            writer.writerow([dma, total_pipe_lengths, total_pipe_lengths_covered, precentage_coverage])
+
+    def update_edge_coverage_fraction(self, outputfile):
+        df = pd.read_csv(outputfile, header=[0])
+        for pipe in df.pipe_id.unique():
+            dftmp = df.loc[df['pipe_id'] == pipe]
+            dftmp = dftmp.drop_duplicates()
+            pipe_len = dftmp['pipe_length'].iloc[0]
+            if len(dftmp) > 1:
+                pipe_cov = dftmp['coverage_length'].sum()
+                total_cov = pipe_cov if pipe_cov < pipe_len else pipe_len
+            else:
+                total_cov = dftmp['coverage_length'].iloc[0]
+
+            start_node_key = dftmp['start_node_key'].iloc[0]
+            end_node_key = dftmp['end_node_key'].iloc[0]
+            if pipe_len>0:
+                cov_fraction = (total_cov / pipe_len)*100
+            else:
+                cov_fraction = 100
+            query = f"""
+            MATCH (n {{node_key: '{start_node_key}'}})-[r]-(m {{node_key: '{end_node_key}'}}) 
+            SET r.coverageFraction = '{cov_fraction}';
+            """
+            db.cypher_query(query)
 
     def compute_cov(self):
         """
-        Converts the Neo4j graph data to WNTR format.
+        Compute coverage.
 
-         """
-        sub_graph  = self.query_graph_dma(self.config.dma_codes)
+        """
+        sub_graph = self.query_graph_dma(self.config.dma_codes)
 
-        print(type(sub_graph))
+        self.initialise_csv()
         
         self.process_logger(sub_graph)
+        
+        self.update_edge_coverage_fraction(self.config.outputfile)
+
+        self.summary_statistics(self.config.outputfile)
 
         
 
