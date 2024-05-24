6f8212a16289d9d36220ceda1ee880c4a9a43cf5
spatialgeobyte <158478685+spatialgeobyte@users.noreply.github.com>
2024-05-24 09:53:26 +0200

    update sprites and fix sprites URL

--- geoserver/setup_styles.sh
+++ geoserver/setup_styles.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-
-cp /settings/* /opt/geoserver/data_dir/styles/
+if [[ ! -d /opt/geoserver/data_dir/styles/ ]];then
+  mkdir -p /opt/geoserver/data_dir/styles/
+fi
+cp -r /styles/* /opt/geoserver/data_dir/styles/
