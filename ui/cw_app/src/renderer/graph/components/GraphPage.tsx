885b21d136049575ddc8dcebbf8bc73a29289bd8
Zaheer <{ID}+{username}@users.noreply.github.com>
2024-05-27 17:49:25 +0200

    added config for neo4j browser

--- ui/cw_app/src/renderer/graph/components/GraphPage.tsx
+++ ui/cw_app/src/renderer/graph/components/GraphPage.tsx
@@ -1,10 +1,11 @@
 import BaseLayout from '../../core/components/BaseLayout';
+import { NEO4J_BROWSER_CONFIG } from '../../config'
 
 export default function GraphPage() {
     return (
         <BaseLayout>
             <iframe
-                src="http://localhost:7475/"
+                src={NEO4J_BROWSER_CONFIG}
                 title="neo4j browser"
                 frameBorder="0"
                 style={{ overflow: 'hidden', height: '100vh', width: '100%' }}
