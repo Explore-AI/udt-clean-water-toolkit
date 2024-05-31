584eb28b07aae31c880ea51f728138b6395d207f
gerald-eai <gerald@explore.ai>
2024-05-31 15:26:56 +0200

    Updated trunkmains schematic viewset to mimic the schematic viewset operations

--- api/api_drf/cw_graph/urls.py
+++ api/api_drf/cw_graph/urls.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import DefaultRouter
-from .viewsets import SchematicViewset
+from .viewsets import SchematicViewset, SchematicTrunkMainViewset
 
 router = DefaultRouter()
 router.register(r"schematic", SchematicViewset, basename="schematic")
+router.register(r"schematic-trunk-main", SchematicTrunkMainViewset, basename="schematic-trunk-main")
 
 urlpatterns = router.urls 
