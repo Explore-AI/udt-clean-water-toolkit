074c136841f2e8207c059675e45aea15e0ee0c2f
gerald-eai <gerald@explore.ai>
2024-06-03 13:22:42 +0200

    Confirmed docker containers run as expected. Scaffolded schematic page.

---
+++ ui/cw_app/src/renderer/schematic/components/SchematicPage.tsx
@@ -0,0 +1,8 @@
+import BaseLayout from "../../core/components/BaseLayout";
+import SchematicView from "./Schematic";
+import React from "react";
+
+
+export default function NodeSchematicPage(){
+    
+}
