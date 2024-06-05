6aec655fdc1bd4140766cd287a70a5419be371d4
marthinus-sandtech <mvanderwalt@sandtech.com>
2024-06-05 15:25:58 +0200

    edited layer to sql files

--- cwa/cwa_geodjango/cwageodjango/core/scripts/layer_connection_meters_to_sql.py
+++ cwa/cwa_geodjango/cwageodjango/core/scripts/layer_connection_meters_to_sql.py
@@ -1,11 +1,11 @@
 from django.core.management.base import BaseCommand
 from django.contrib.gis.gdal import DataSource
-from cwageodjango.assets.models import TrunkMain
+from cwageodjango.assets.models import ConnectionMeter
 from cwageodjango.utilities.models import DMA
 
 
 class Command(BaseCommand):
-    help = "Write Thames Water trunk mains layer data to sql"
+    help = "Write Thames Water connection meter layer data to sql"
 
     def add_arguments(self, parser):
         parser.add_argument("-f", "--file", type=str, help="Path to valid datasource")
@@ -22,34 +22,32 @@ class Command(BaseCommand):
 Large numbers of features will take a long time to save."""
         )
 
-        trunk_main_layer = ds[layer_index]
+        connection_meter_layer = ds[layer_index]
 
-        new_trunk_mains = []
-        for feature in trunk_main_layer:
+        new_connection_meters = []
+        for feature in connection_meter_layer:
             gid = feature.get("GISID")
             geom = feature.geom
             geom_4326 = feature.get("wkt_geom_4326")
-            material = feature.get("MATERIAL") or "unknown"
-            diameter = feature.get("DIAMETER_mm")
+            subtype = feature.get("SUBTYPE")
 
-            new_trunk_main = TrunkMain(
-                gid=gid, geometry=geom.wkt, geometry_4326=geom_4326, material=material, diameter=diameter
+            new_connection_meter = ConnectionMeter(
+                gid=gid, geometry=geom.wkt, geometry_4326=geom_4326, subtype=subtype
             )
-            new_trunk_mains.append(new_trunk_main)
+            new_connection_meters.append(new_connection_meter)
 
-            if len(new_trunk_mains) == 100000:
-                TrunkMain.objects.bulk_create(new_trunk_mains)
-                new_trunk_mains = []
+            if len(new_connection_meters) == 100000:
+                ConnectionMeter.objects.bulk_create(new_connection_meters)
+                new_connection_meters = []
 
         # save the last set of data as it will probably be less than 100000
-        if new_trunk_mains:
-            TrunkMain.objects.bulk_create(new_trunk_mains)
+        if new_connection_meters:
+            ConnectionMeter.objects.bulk_create(new_connection_meters)
 
-        DMAThroughModel = TrunkMain.dmas.through
+        DMAThroughModel = ConnectionMeter.dmas.through
         bulk_create_list = []
-        for trunk_main in TrunkMain.objects.only("id", "geometry"):
-
-            wkt = trunk_main.geometry.wkt
+        for connection_meter in ConnectionMeter.objects.only("id", "geometry"):
+            wkt = connection_meter.geometry.wkt
 
             dma_ids = DMA.objects.filter(geometry__intersects=wkt).values_list(
                 "pk", flat=True
@@ -58,10 +56,12 @@ Large numbers of features will take a long time to save."""
             if not dma_ids:
                 dma_ids = [DMA.objects.get(name=r"undefined").pk]
 
-            for dma_id in dma_ids:
-                bulk_create_list.append(
-                    DMAThroughModel(trunkmain_id=trunk_main.pk, dma_id=dma_id)
-                )
+            bulk_create_list.extend(
+                [
+                    DMAThroughModel(connectionmeter_id=connection_meter.pk, dma_id=dma_id)
+                    for dma_id in dma_ids
+                ]
+            )
 
             if len(bulk_create_list) == 100000:
                 DMAThroughModel.objects.bulk_create(bulk_create_list)
