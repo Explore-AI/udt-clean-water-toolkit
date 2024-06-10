e1fdcd83753dc738fbf220ff6e95c09dea406f45
spatialgeobyte <mazano@spatialbyte.Dlink>
2024-06-10 08:50:12 +0200

    fix map styling

--- ui/cw_app/src/renderer/map/utils/mapUtils.ts
+++ ui/cw_app/src/renderer/map/utils/mapUtils.ts
@@ -1,8 +1,8 @@
-import { GEOSERVER_URL } from '../../config';
 import { MapViewState } from '@deck.gl/core';
 import { MVTLayer } from '@deck.gl/geo-layers';
-import {colorCategories, colorContinuous} from '@deck.gl/carto';
+import { colorCategories } from '@deck.gl/carto';
 import { map as _map, isEmpty as _isEmpty } from 'lodash';
+import { GEOSERVER_URL } from '../../config';
 
 export type LayerNames = {
     [key: string]: string;
@@ -11,7 +11,6 @@ export type LayerNames = {
 export type LayerColorCodes = {
     [key: string]: number[];
 };
-
 
 export const MVT_LAYER_URL = (asset_name: string) => {
     return `${GEOSERVER_URL}/geoserver/gwc/service/tms/1.0.0/udt:${asset_name}@EPSG:3857@pbf/{z}/{x}/{-y}.pbf`;
@@ -117,29 +116,201 @@ export const gpsRegex =
     /^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?((1[0-7]\d(\.\d+)?|180(\.0+)?)|(\d{1,2}(\.\d+)?))$/;
 export const gisidRegex = /^\d{7}$/;
 
+function getFillColorFeatures(feature, colorMap) {
+    const { property } = colorMap;
+    const value = feature.properties[property];
+    return colorMap.colors[value] || colorMap.default;
+}
+
+const AssetsPressurefittingColormap = {
+    property: 'subtype',
+    colors: {
+        tapping_point: [220, 215, 71],
+        telemetry_pit: [15, 24, 255, 128],
+        transducer: [0, 0, 255, 128],
+    },
+    default: [0, 255, 0, 128],
+};
+
+const AssetsChamberColormap = {
+    property: 'gid',
+    colors: {
+        Null: [220, 215, 71],
+    },
+    default: [76, 43, 50, 1],
+};
+
+const AssetsHydrantColormap = {
+    property: 'gid',
+    colors: {
+        Null: [220, 215, 71],
+    },
+    default: [145, 82, 45, 1],
+};
+
+const AssetsLoggerColormap = {
+    property: 'gid',
+    colors: {
+        Null: [220, 215, 71],
+    },
+    default: [100, 81, 44, 1],
+};
+
+const AssetsNetworkmeterColormap = {
+    property: 'subtype',
+    colors: {
+        distribution_input_meter: [220, 212, 71],
+        district_meter: [146, 24, 17, 128],
+        fire_meter: [10, 0, 77, 128],
+        unknown: [80, 102, 200, 128],
+        waste_meter: [90, 40, 120, 128],
+        zonal_meter: [60, 50, 133, 128],
+    },
+    default: [0, 255, 0, 128],
+};
+
+const AssetsNetworkoptvalveColormap = {
+    property: 'gid',
+    colors: {
+        Null: [220, 215, 71],
+    },
+    default: [88, 43, 50, 1],
+};
+
+const AssetsOperationalsiteColormap = {
+    property: 'subtype',
+    colors: {
+        booster_station: [128, 233, 102],
+        borehole: [61, 202, 164],
+        break_pressure_tank: [209, 212, 38],
+        other: [130, 222, 157],
+        pumping_station: [174, 214, 126],
+        raw_water_storage_reservoir: [117, 157, 218],
+        reservoir_covered: [59, 195, 225],
+        service_reservoir: [237, 47, 95],
+        shaft_inspection: [108, 103, 231],
+        shaft_pumping: [232, 194, 236],
+        treatment_works: [236, 42, 174],
+        unknown: [201, 109, 213],
+        water_tower: [162, 96, 237],
+    },
+    default: [0, 255, 0, 128],
+};
+
+const AssetsPressurecontrolvalveColormap = {
+    property: 'subtype',
+    colors: {
+        needle: [211, 223, 45],
+        non_return_valve: [202, 79, 73],
+        plug_valve: [41, 29, 221],
+        pressure_reducing: [240, 144, 234],
+        pressure_sustaining: [77, 198, 207],
+    },
+    default: [0, 255, 0, 128],
+};
+
+
+function AssetsPressurecontrolvalveFillcolor(feature) {
+    return getFillColorFeatures(feature, AssetsPressurefittingColormap);
+}
+
+function AssetsChamberFillcolor(feature) {
+    return getFillColorFeatures(feature, AssetsChamberColormap);
+}
+
+function AssetsHydrantFillcolor(feature) {
+    return getFillColorFeatures(feature, AssetsHydrantColormap);
+}
+
+function AssetsLoggerFillcolor(feature) {
+    return getFillColorFeatures(feature, AssetsLoggerColormap);
+}
+
+function AssetsNetworkmeterFillcolor(feature) {
+    return getFillColorFeatures(feature, AssetsNetworkmeterColormap);
+}
+
+function AssetsNetworkoptvalveFillcolor(feature) {
+    return getFillColorFeatures(feature, AssetsNetworkoptvalveColormap);
+}
+
+function AssetsOperationalsiteFillcolor(feature) {
+    return getFillColorFeatures(feature, AssetsOperationalsiteColormap);
+}
+
+function AssetsPressureControlValveFillcolor(feature) {
+    return getFillColorFeatures(feature, AssetsPressurecontrolvalveColormap);
+}
+
 export const COMMON_STYLING = {
-    opacity: 0.1
-}
+    minZoom: 8,
+    maxZoom: 18,
+};
 
-export const POINT_STYLING = {
-    ...COMMON_STYLING,
-    getFillColor: [1,1,1],
-}
+export const POINT_STYLING = (AssetName: string) => {
+    let styling = '';
+    let AssetsValues = [
+        'assets_pressurefitting',
+        'assets_chamber',
+        'assets_hydrant',
+        'assets_logger',
+        'assets_networkmeter',
+        'assets_networkoptvalve',
+        'assets_operationalsite',
+        'assets_pressurecontrolvalve',
+    ];
 
-export const LINE_STYLING = {
-    ...COMMON_STYLING,
-    getFillColor: [1,1,1],
-}
+    if (AssetName === 'assets_pressurefitting') {
+        styling = AssetsPressurecontrolvalveFillcolor;
+    } else if (AssetName === 'assets_chamber') {
+        styling = AssetsChamberFillcolor;
+    } else if (AssetName === 'assets_hydrant') {
+        styling = AssetsHydrantFillcolor;
+    } else if (AssetName === 'assets_logger') {
+        styling = AssetsLoggerFillcolor;
+    } else if (AssetName === 'assets_networkmeter') {
+        styling = AssetsNetworkmeterFillcolor;
+    } else if (AssetName === 'assets_networkoptvalve') {
+        styling = AssetsNetworkoptvalveFillcolor;
+    } else if (AssetName === 'assets_operationalsite') {
+        styling = AssetsOperationalsiteFillcolor;
+    } else if (AssetName === 'assets_pressurecontrolvalve') {
+        styling = AssetsPressureControlValveFillcolor;
+    }
+    return {
+        ...COMMON_STYLING,
+        radiusMinPixels: 6,
+        radiusMaxPixels: 10,
+        getPointRadius: 20,
+        getFillColor: f => styling(f),
+    };
+};
+export const LINE_STYLING = (AssetName: string) => {
+    let ColorValue: number[] = [];
+    if (AssetName === 'assets_distributionmain') {
+        ColorValue = [228, 135, 60];
+    } else if (AssetName === 'assets_trunkmain') {
+        ColorValue = [72, 123, 182];
+    }
+
+    return {
+        ...COMMON_STYLING,
+        getLineColor: ColorValue,
+        lineWidthMinPixels: 1.5,
+        stroked: false,
+    };
+};
 
 export const POLYGON_STYLING = {
     ...COMMON_STYLING,
+    opacity: 0.1,
     getFillColor: colorCategories({
         attr: 'code',
-        domain: ['ZABINB01', 'ZABIND03', 'ZCHIPO01', 'mid and military', 'ZABIND08'],// TODO: get dmas from from geoserverb
-        colors: 'BluYl'
+        domain: ['ZABINB01', 'ZABIND03', 'ZCHIPO01', 'ZABIND08'], // TODO: get dmas from from geoserver
+        colors: 'BluYl',
+        othersColor: [72, 123, 182],
     }),
-}
-
+};
 
 export const createLayers = (newMapLayerProps = []) => {
     const mapLayerProps = !_isEmpty(newMapLayerProps)? newMapLayerProps: MAP_LAYERS_PROPS;
@@ -148,11 +319,11 @@ export const createLayers = (newMapLayerProps = []) => {
 
         let layerStyle = COMMON_STYLING
         if (layerProps.assetType === 'point') {
-            layerStyle = POINT_STYLING
+            layerStyle = POINT_STYLING(layerProps.key);
         } else if (layerProps.assetType === 'line') {
-            layerStyle = LINE_STYLING
+            layerStyle = LINE_STYLING(layerProps.key);
         } else if (layerProps.assetType === 'polygon') {
-            layerStyle = POLYGON_STYLING
+            layerStyle = POLYGON_STYLING;
         }
 
         return new MVTLayer({
@@ -160,7 +331,7 @@ export const createLayers = (newMapLayerProps = []) => {
             id: layerProps.key,
             data: [MVT_LAYER_URL(layerProps.key)],
             visible: layerProps.visible,
-            ...layerStyle
+            ...layerStyle,
         });
     });
 };
