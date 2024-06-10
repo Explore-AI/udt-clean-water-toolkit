57fdeac6413be45a276c35e84b2f8f835d850b3e
gerald-eai <gerald@explore.ai>
2024-06-10 14:11:52 +0200

    Added a couple more icons

---
+++ ui/cw_app/src/renderer/schematic/components/IconComponents/ServiceReservoir.tsx
@@ -0,0 +1,22 @@
+export const DefaultSresIcon = () => {
+    return (
+        <svg
+            width="50"
+            height="50"
+            viewBox="0 0 50 50"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <rect
+                x="4"
+                y="9"
+                width="42"
+                height="32"
+                rx="3"
+                stroke="white"
+                stroke-width="2"
+            />
+            <rect x="5" y="10" width="40" height="30" rx="2" fill="#009EDE" />
+        </svg>
+    );
+};
