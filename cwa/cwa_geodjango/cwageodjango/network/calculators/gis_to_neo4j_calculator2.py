f007e1e3578f757625accc0d3fee94e3a14780c5
Zaheer <{ID}+{username}@users.noreply.github.com>
2024-06-10 10:19:54 +0200

    refactor cpyher

--- cwa/cwa_geodjango/cwageodjango/network/calculators/gis_to_neo4j_calculator2.py
+++ cwa/cwa_geodjango/cwageodjango/network/calculators/gis_to_neo4j_calculator2.py
@@ -42,83 +42,6 @@ class GisToNeo4jCalculator2(GisToGraph):
             neoj4_point=self.config.neoj4_point,
         )
 
-    def _connect_nodes(self, edge_by_pipe, start_node, end_node):
-        asset_label = edge_by_pipe["asset_label"]
-
-        match_query = f"""match (n:NetworkNode {{node_key:'{start_node['node_key']}'}})-
-        [r:{asset_label}]-
-        (m:NetworkNode {{node_key:'{end_node['node_key']}'}}) return count(r)
-        """
-
-        if db.cypher_query(match_query)[0][0][0] == 0:
-            query = f"""match (n:NetworkNode {{node_key:'{start_node['node_key']}'}}),
-            (m:NetworkNode {{node_key:'{end_node['node_key']}'}})
-            create (n)-[:{asset_label} {{
-            gid: {edge_by_pipe["gid"]},
-            material: '{edge_by_pipe["material"]}',
-            diameter: '{edge_by_pipe["diameter"]}',
-            segment_wkt: '{edge_by_pipe["segment_wkt"]}',
-            segment_length: {edge_by_pipe["segment_length"]}
-            }}]->(m)"""
-
-            db.cypher_query(query)
-
-    @staticmethod
-    def _get_node_by_key(node_key):
-        network_node = db.cypher_query(
-            f"""match (n:NetworkNode {{node_key:'{node_key}'}})
-        return n"""
-        )[0]
-
-        if network_node:
-            return network_node[0][0]
-
-        return None
-
-    @staticmethod
-    def create_node_asset_query(node_properties):
-        props = f"""
-        coords_27700: {node_properties['coords_27700']},
-        node_key:'{node_properties['node_key']}',
-        node_types: {node_properties['node_types']},
-        asset_names: {node_properties['point_asset_names']},
-        asset_gids: {node_properties['point_asset_gids']}
-        """
-
-        asset_gids = [
-            f"{key}: {val}"
-            for key, val in node_properties["point_assets_with_gids"].items()
-        ]
-
-        acoustic_logger = node_properties.get("acoustic_logger")
-        subtype = node_properties.get("subtype")
-
-        query = f"CREATE (n:{('&').join(node_properties['node_labels'])} {{"
-        query += props
-
-        query += f",{(', ').join(asset_gids)}"
-
-        if acoustic_logger:
-            query += f", acoustic_logger: '{acoustic_logger}'"
-
-        if subtype:
-            query += f", subtype: '{subtype}'"
-
-        query += f"}}) return n"
-
-        return query
-
-    @staticmethod
-    def create_pipe_node_query(node_properties):
-        query = f"""CREATE (
-        n:{('&').join(node_properties['node_labels'])}
-        {{utility:'{node_properties['utility']}',
-        coords_27700: {node_properties['coords_27700']},
-        node_key:'{node_properties['node_key']}'
-        }}) return n
-        """
-        return query
-
     def _get_or_create_dma_node(self, dma_code, dma_name):
         """Create or get a DMA node."""
 
@@ -136,30 +59,6 @@ class GisToNeo4jCalculator2(GisToGraph):
 
         result = db.cypher_query(query)
         return result[0][0][0]
-
-    def _get_or_create_pipe_and_asset_node(self, node_properties):
-        try:
-            query = self.create_node_asset_query(node_properties)
-            return db.cypher_query(query)[0][0][0]
-
-        except UniqueProperty:
-            return self._get_node_by_key(node_properties["node_key"])
-
-    def _get_or_create_point_asset_node(self, node_properties):
-        try:
-            query = self.create_node_asset_query(node_properties)
-            return db.cypher_query(query)[0][0][0]
-
-        except UniqueProperty:
-            return self._get_node_by_key(node_properties["node_key"])
-
-    def _create_pipe_junction_or_end_node(self, node_properties):
-        try:
-            query = self.create_pipe_node_query(node_properties)
-            return db.cypher_query(query)[0][0][0]
-
-        except UniqueProperty:
-            return self._get_node_by_key(node_properties["node_key"])
 
     def set_dynamic_node_properties(self):
 
@@ -194,7 +93,7 @@ class GisToNeo4jCalculator2(GisToGraph):
 
         return subquery
 
-    def _create_nodes(self, all_unique_nodes):
+    def _batch_create_network_nodes(self, all_unique_nodes):
 
         query = f"""UNWIND $all_unique_nodes AS node
          MERGE (n:NetworkNode {{node_key: node.node_key}})
@@ -221,16 +120,26 @@ class GisToNeo4jCalculator2(GisToGraph):
             dma_node = self._get_or_create_dma_node(dma_code, dma_name)
             self._create_dma_relationship(node, dma_node)
 
-    def _create_relations(self, edges_by_pipe, all_nodes):
-        current_node = all_nodes[0]
+    def _batch_create_pipe_relations(self, all_unique_edges):
 
-        for next_node, edge_by_pipe in zip(all_nodes[1:], edges_by_pipe):
-            sorted_nodes = sorted(
-                [current_node, next_node], key=lambda node: node["node_key"]
-            )
+        query = """
+        UNWIND $all_unique_edges AS edge
+        MATCH (n:NetworkNode {node_key: edge.from_node_key}),
+        (m:NetworkNode {node_key: edge.to_node_key})
+        MERGE (n)-[r:PIPE_MAIN {
+            gid: edge.gid,
+            material: edge.material,
+            diameter: edge.diameter,
+            segment_wkt: edge.segment_wkt,
+            segment_length: edge.segment_length
+            }]-(m)
+        ON CREATE SET
+        RETURN r
+        """
+        import pdb
 
-            self._connect_nodes(edge_by_pipe, sorted_nodes[0], sorted_nodes[1])
-            current_node = next_node
+        pdb.set_trace()
+        db.cypher_query(query, {"all_unique_nodes": all_unique_edges})
 
     def _create_dma_relationship(self, node, dma_node):
         query = f"""MATCH (n:NetworkNode {{node_key: '{node["node_key"]}'}}), (d:DMA {{code: '{dma_node["code"]}'}})
@@ -245,8 +154,8 @@ class GisToNeo4jCalculator2(GisToGraph):
     def _map_pipe_connected_asset_relations(
         self, all_unique_nodes: list, all_unique_edges: list
     ):
-        all_nodes = self._create_nodes(all_unique_nodes)
-        self._create_relations(edges_by_pipe, all_nodes)
+        self._batch_create_network_nodes(all_unique_nodes)
+        # self._batch_create_pipe_relations(all_unique_edges)
 
     def _get_unique_nodes_and_edges(self):
         all_nodes = flatten_concatenation(self.all_nodes_by_pipe)
