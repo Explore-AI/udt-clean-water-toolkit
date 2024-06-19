bf0d64dcb64cb7c861a4dc1ad535efabd0bf1266
Zaheer <{ID}+{username}@users.noreply.github.com>
2024-06-19 10:00:49 +0200

    fix to neo4j backup

--- devops/scripts/neo4j/neo4j_db_backup.sh
+++ devops/scripts/neo4j/neo4j_db_backup.sh
@@ -10,7 +10,7 @@ if [ ! -d ${DB_BACKUPS_DIR} ]; then
     mkdir -p ${DB_BACKUPS_DIR}
 fi
 
-DB_CONTAINER_ID=`docker ps | grep udtneo4j | grep neo4j:5.18.0-community-bullseye | awk '{ print $1 }'`
+DB_CONTAINER_ID=`docker ps | grep udtneo4j | grep neo4j:5.20.0-community-bullseye | awk '{ print $1 }'`
 
 CURRENT_DATETIME=`date "+%d-%m-%Y_%H-%M-%S"`
 BACKUP_FILE_NAME=${DB_BACKUPS_DIR}/udt_neo4j_db_backup_${CURRENT_DATETIME}.dump
@@ -20,7 +20,7 @@ if [[ -z ${DB_CONTAINER_ID} ]];then
 else
   docker stop ${DB_CONTAINER_ID}
   #docker run -it  --rm --env-file ../../docker/env_files/.db_env --volume=docker_neo4j-data:/data --volume=docker_neo4j:/backups neo4j/neo4j-admin neo4j-admin database dump neo4j --verbose --to-path=/backups
-  docker run -it --rm --env-file ../../docker/env_files/.db_env --volume=docker_neo4j-data:/data --volume=docker_neo4j-backups:/var/lib/neo4j/backups/ neo4j:5.18.0-community-bullseye bin/neo4j-admin database dump neo4j --verbose --to-path=./backups/ --overwrite-destination=true
+  docker run -it --rm --env-file ../../docker/env_files/.db_env --volume=docker_neo4j-data:/data --volume=docker_neo4j-backups:/var/lib/neo4j/backups/ neo4j:5.20.0-community-bullseye bin/neo4j-admin database dump neo4j --verbose --to-path=./backups/ --overwrite-destination=true
   docker start ${DB_CONTAINER_ID}
   docker cp ${DB_CONTAINER_ID}:/backups/neo4j.dump ${BACKUP_FILE_NAME}
 fi
--- devops/scripts/neo4j/neo4j_db_restore.sh
+++ devops/scripts/neo4j/neo4j_db_restore.sh
@@ -5,7 +5,7 @@ echo
 
 OPTSTRING=":f:"
 
-DB_CONTAINER_ID=`docker ps | grep udtneo4j | grep neo4j:5.18.0-community-bullseye | awk '{ print $1 }'`
+DB_CONTAINER_ID=`docker ps | grep udtneo4j | grep neo4j:5.20.0-community-bullseye | awk '{ print $1 }'`
 
 while getopts ${OPTSTRING} opt; do
     case ${opt} in
@@ -17,7 +17,7 @@ while getopts ${OPTSTRING} opt; do
             docker stop ${DB_CONTAINER_ID}
             # Start the backup container
             # docker run -it --rm --env-file ../../docker/env_files/.db_env --volume=docker_neo4j-data:/data --volume=docker_neo4j-backup:/backups neo4j/neo4j-admin neo4j-admin database load neo4j --from-path=/backups/ --verbose --overwrite-destination=true
-            docker run -it --rm --env-file ../../docker/env_files/.db_env --volume=docker_neo4j-data:/data --volume=docker_neo4j-backups:/var/lib/neo4j/backups/ neo4j:5.18.0-community-bullseye bin/neo4j-admin database load neo4j --from-path=/var/lib/neo4j/backups/ --verbose --overwrite-destination=true
+            docker run -it --rm --env-file ../../docker/env_files/.db_env --volume=docker_neo4j-data:/data --volume=docker_neo4j-backups:/var/lib/neo4j/backups/ neo4j:5.20.0-community-bullseye bin/neo4j-admin database load neo4j --from-path=/var/lib/neo4j/backups/ --verbose --overwrite-destination=true
             docker start ${DB_CONTAINER_ID}
             echo
             echo "udt neo4j DB restore complete."
