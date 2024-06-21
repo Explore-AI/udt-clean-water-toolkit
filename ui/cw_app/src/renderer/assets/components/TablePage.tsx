8a75a55beeda32dff241926b079d0b1e5065e731
gerald-eai <gerald@explore.ai>
2024-06-21 13:07:59 +0200

    code cleanup and prettier formatting

--- ui/cw_app/src/renderer/assets/components/TablePage.tsx
+++ ui/cw_app/src/renderer/assets/components/TablePage.tsx
@@ -5,12 +5,11 @@ import styles from '../css/TablePage.module.css';
 import useFetchJson from '../../core/hooks/useFetchJson';
 import withTable from '../hoc/withTable';
 import { TableContext } from '../hooks/useTableUi';
-import { ScrollArea } from '@mantine/core';
 
 function TablePage(props: PageProps) {
     // set the default key for fetching the data
-    const { selectedKey, page_size, page } = useContext(TableContext);
-    useFetchJson(selectedKey, { page_size: page_size, page: page });
+    const { selectedKey } = useContext(TableContext);
+    useFetchJson(selectedKey);
 
     const { pageVisibility } = props;
     const mainCss = `${styles.main} ${styles[pageVisibility]}`;
@@ -18,9 +17,7 @@ function TablePage(props: PageProps) {
     return (
         <>
             <div className={mainCss}>
-                <ScrollArea>
-                    <TableView />
-                </ScrollArea>
+                <TableView />
             </div>
         </>
     );
