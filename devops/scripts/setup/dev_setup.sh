5f831c04d9fcd5acc73443f52da51c9809b5f627
Zaheer <{ID}+{username}@users.noreply.github.com>
2024-06-26 09:41:27 +0200

    fix to packages

--- devops/scripts/setup/dev_setup.sh
+++ devops/scripts/setup/dev_setup.sh
@@ -11,7 +11,7 @@ echo "pip packages will be installed for the cwm in dev mode."
 echo
 
 
-docker compose -f ../../docker/docker-compose-postgis.yml -f ../../docker/docker-compose-neo4j.yml -f ../../docker/docker-compose-cwa-geodjango-dev.yml -f ../../docker/docker-compose-cwa-geoalchemy-dev.yml -f ../../docker/docker-compose-api-drf.yml up -d
+docker compose -f ../../docker/docker-compose-postgis.yml -f ../../docker/docker-compose-neo4j.yml -f ../../docker/docker-compose-cwa-geodjango-dev.yml -f ../../docker/docker-compose-cwa-geoalchemy-dev.yml -f ../../docker/docker-compose-api-drf.yml up -d --build
 
 CWA_GEODORM_CONTAINER_ID=`docker ps | grep udtcwageodjangodev | grep cwa_geodjango_dev | awk '{ print $1 }'`
 CWA_GEOALCHEMY_CONTAINER_ID=`docker ps | grep udtcwageoalchemydev | grep cwa_geoalchemy_dev | awk '{ print $1 }'`
