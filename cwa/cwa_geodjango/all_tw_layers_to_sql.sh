8b5250f4d958113446818312ac79e211eb062335
Zaheer <{ID}+{username}@users.noreply.github.com>
2024-07-04 16:54:50 +0200

    updating ingestion scripts

--- cwa/cwa_geodjango/all_tw_layers_to_sql.sh
+++ cwa/cwa_geodjango/all_tw_layers_to_sql.sh
@@ -1,15 +1,17 @@
 
-DMA_LAYER_INDEX=0
-WNETWORKMETER_LAYER_INDEX=1
-WPRESSURECONTVALVE_LAYER_INDEX=2
-WHYDRANT_LAYER_INDEX=3
-PIPES_LAYER_INDEX=4
+PIPES_LAYER_INDEX=0
+WCHAMBER_LAYER_INDEX=1
+WCONNECTIONMETER_LAYER_INDEX=2
+WCONSUMPTIONMETER_LAYER_INDEX=3
+WHYDRANT_LAYER_INDEX=4
 WLOGGER_LAYER_INDEX=5
-WPRESSUREFITTING_LAYER_INDEX=6
-WOPERATIONALSITE_LAYER_INDEX=7
-WCHAMBER_LAYER_INDEX=8
-WNETWORKOPTVALVE_LAYER_INDEX=9
-
+WNETWORKMETER_LAYER_INDEX=6
+WNETWORKOPTVALVE_LAYER_INDEX=7
+WOPERATIONALSITE_LAYER_INDEX=8
+WPRESSURECONTVALVE_LAYER_INDEX=9
+WPRESSUREFITTING_LAYER_INDEX=10
+DMA_LAYER_INDEX=11
+ACOUSTIC_LOGGER_STATUS_MAPPING_LAYER_INDEX=12
 
 OPTSTRING=":f:"
 
@@ -17,15 +19,15 @@ while getopts ${OPTSTRING} opt; do
     case ${opt} in
         f)
             python3 manage.py layer_tw_dmas_to_sql -f ${OPTARG} -x ${DMA_LAYER_INDEX}
-            # python3 manage.py layer_tw_network_meters_to_sql -f ${OPTARG} -x ${WNETWORKMETER_LAYER_INDEX}
-            # python3 manage.py layer_tw_pressure_control_valve_to_sql -f ${OPTARG} -x ${WPRESSURECONTVALVE_LAYER_INDEX}
+            python3 manage.py layer_tw_network_meters_to_sql -f ${OPTARG} -x ${WNETWORKMETER_LAYER_INDEX}
+            python3 manage.py layer_tw_pressure_control_valve_to_sql -f ${OPTARG} -x ${WPRESSURECONTVALVE_LAYER_INDEX}
             python3 manage.py layer_tw_hydrants_to_sql -f ${OPTARG} -x ${WHYDRANT_LAYER_INDEX}
             python3 manage.py layer_tw_mains_to_sql -f ${OPTARG} -x ${PIPES_LAYER_INDEX}
             python3 manage.py layer_tw_loggers_to_sql -f ${OPTARG} -x ${WLOGGER_LAYER_INDEX}
-            #python3 manage.py layer_tw_pressure_fittings_to_sql -f ${OPTARG} -x ${WPRESSUREFITTING_LAYER_INDEX}
-            #python3 manage.py layer_tw_operational_site_to_sql -f ${OPTARG} -x ${WOPERATIONALSITE_LAYER_INDEX}
+            python3 manage.py layer_tw_pressure_fittings_to_sql -f ${OPTARG} -x ${WPRESSUREFITTING_LAYER_INDEX}
+            python3 manage.py layer_tw_operational_site_to_sql -f ${OPTARG} -x ${WOPERATIONALSITE_LAYER_INDEX}
             python3 manage.py layer_tw_chambers_to_sql -f ${OPTARG} -x ${WCHAMBER_LAYER_INDEX}
-            #python3 manage.py layer_tw_network_opt_valve_to_sql -f ${OPTARG} -x ${WNETWORKOPTVALVE_LAYER_INDEX}
+            python3 manage.py layer_tw_network_opt_valve_to_sql -f ${OPTARG} -x ${WNETWORKOPTVALVE_LAYER_INDEX}
             ;;
     esac
 done
