bcd4da097cc3e8b00660a4600b1e10b96fd6ac4d
GerhardtB <gerhardtbotha5@gmail.com>
2024-07-24 16:36:25 +0200

    add pk, tag, timestamp filter

--- api/api_drf/cw_gis_assets/viewsets/pipeflow_viewset.py
+++ api/api_drf/cw_gis_assets/viewsets/pipeflow_viewset.py
@@ -1,30 +1,33 @@
-from django.shortcuts import get_object_or_404
+from django_filters import rest_framework as filters
+from django.db.models import JSONField
+from cwageodjango.waterpipes.models import PipeFlow
+from config.filters import BaseFilter
 from config.viewsets import BaseModelViewSet
 from cwageodjango.waterpipes.models import PipeFlow
 from cw_gis_assets.serializers import PipeFlowSerializer
-from rest_framework.decorators import action
-from rest_framework.response import Response
-from rest_framework import status
 
 
-class PipeFlowViewSet(BaseModelViewSet):
-    serializer_class = PipeFlowSerializer
-    queryset = PipeFlow.objects.all()
-    http_method_names = ["get"]
+class PipeFlowFilter(BaseFilter):
+    tag = filters.CharFilter(field_name="pipe_main__tag", lookup_expr="icontains")
+    ids = filters.BaseInFilter(field_name="pipe_main__id", lookup_expr="in")
+    flow_data_timestamp = filters.CharFilter(method="filter_flow_data_timestamp")
 
     class Meta:
         model = PipeFlow
-        fields = ["pipe_main", "flow_data"]
+        fields = ["pipe_main", "flow_data", "ids", "tag", "flow_data_timestamp"]
+        filter_overrides = {
+            JSONField: {
+                "filter_class": filters.CharFilter,
+            },
+        }
 
-    def list(self, request, *args, **kwargs):
-        sample_size = 10  # Define the number of samples you want to return
-        queryset = self.get_queryset()[
-            :sample_size
-        ]  # Get only the first `sample_size` results
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    def filter_flow_data_timestamp(self, queryset, name, value):
+        lookup_value = value
+        return queryset.filter(flow_data__contains=lookup_value)
 
-    @action(detail=True, methods=["get"], url_path="flow-data", url_name="flow_data")
-    def get_flow_data(self, request, pk=None):
-        pipe_flow = get_object_or_404(PipeFlow, pk=pk)
-        return Response(pipe_flow.flow_data, status=status.HTTP_200_OK)
+
+class PipeFlowViewSet(BaseModelViewSet):
+    queryset = PipeFlow.objects.all()
+    serializer_class = PipeFlowSerializer
+    filterset_class = PipeFlowFilter
+    http_method_names = ["get"]
