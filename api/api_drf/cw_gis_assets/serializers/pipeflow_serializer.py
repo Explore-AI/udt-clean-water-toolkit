a880dd7caa4d3add0b97278e93d0a94b6fbfc5fa
GerhardtB <gerhardtbotha5@gmail.com>
2024-07-24 16:36:25 +0200

    add pk, tag, timestamp filter

--- api/api_drf/cw_gis_assets/serializers/pipeflow_serializer.py
+++ api/api_drf/cw_gis_assets/serializers/pipeflow_serializer.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
+from cwageodjango.assets.models import PipeMain
 from cwageodjango.waterpipes.models import PipeFlow
 
 
+class PipeMainSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = PipeMain
+        fields = ["tag", "dmas", "id"]
+
+
 class PipeFlowSerializer(serializers.ModelSerializer):
+    pipe_main = PipeMainSerializer()
+    tag = serializers.CharField(source="pipe_main.tag", read_only=True)
+    id = serializers.CharField(source="pipe_main.id", read_only=True)
 
     class Meta:
         model = PipeFlow
-        fields = ["pipe_main", "flow_data"]
+        fields = ["pipe_main", "flow_data", "id", "tag"]
